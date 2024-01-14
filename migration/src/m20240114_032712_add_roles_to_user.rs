581e58154ad9ad5339e3021b1a98753afbc47862
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-14 00:56:59 -0300

    build: add role to user schema

---
+++ migration/src/m20240114_032712_add_roles_to_user.rs
@@ -0,0 +1,73 @@
+use sea_orm_migration::{prelude::*, sea_orm::EnumIter, sea_query::extension::postgres::Type};
+use sea_orm::Iterable;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+        .create_type(
+            Type::create()
+            .as_enum(Role::Table)
+            .values(Role::iter().skip(1))
+            .to_owned()
+        ).await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(User::Table)
+                    .add_column_if_not_exists(ColumnDef::new(User::Role).enumeration(Role::Table, Role::iter().skip(1)))
+                    .to_owned()
+            ).await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(User::Table)
+                    .drop_column(Role::Table)
+                    .to_owned()
+            ).await?;
+
+        manager
+            .drop_type(
+                Type::drop()
+                .if_exists()
+                .name(Role::Table)
+                .to_owned()
+            ).await?;
+
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    Role
+}
+
+#[derive(Iden, EnumIter)]
+pub enum Role {
+    Table,
+    #[iden = "User"]
+    User,
+    #[iden = "Writter"]
+    Writter,
+    #[iden = "Editor"]
+    Editor,
+    #[iden = "Coord"]
+    Coord,
+    #[iden = "Admin"]
+    Admin,
+    #[iden = "Principal"]
+    Principal,
+    #[iden = "Ceo"]
+    Ceo
+}
