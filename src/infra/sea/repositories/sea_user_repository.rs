8069e2504c6ecdcd2c9d61b5f76c45bdc3f4010f
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-14 00:25:28 -0300

    feat: sea user repository - refactor - add find_by_nickname method

--- src/infra/sea/repositories/sea_user_repository.rs
+++ src/infra/sea/repositories/sea_user_repository.rs
@@ -1,37 +1,43 @@
-use sea_orm::{EntityTrait, ActiveModelTrait, IntoActiveValue};
+use sea_orm::{EntityTrait, ActiveModelTrait, IntoActiveValue, DbErr, QueryFilter, ColumnTrait};
+use entities::user::Model as UserModel;
 
-use crate::{repositories::user_repository::{UserRepository, CreateUserParam}, infra::sea::sea_service::SeaService};
+use crate::infra::sea::sea_service::SeaService;
 
 pub struct SeaUserRepository {
-    // db_conn: DatabaseConnection,
-    sea_service: SeaService
+    pub sea_service: SeaService,
 }
 
 impl SeaUserRepository {
-    // constructor
-    pub async fn new(service: &SeaService) -> Self {
+    pub async fn new(sea_service: SeaService) -> SeaUserRepository {
+
         SeaUserRepository {
-            sea_service: service.clone(),
+            sea_service,
         }
     }
 }
 
-impl UserRepository for SeaUserRepository {
-    async fn create(&self, params: CreateUserParam) -> Result<entities::user::Model, sea_orm::DbErr> {
+impl SeaUserRepository {
+    pub async fn create(&self, nickname: String, password: String) -> Result<UserModel, DbErr> {
         use uuid::Uuid;
 
         let new_user = entities::user::ActiveModel {
             id: Uuid::new_v4().into_active_value(),
-            nickname: params.nickname.into_active_value(),
-            password: params.password.into_active_value(),
+            nickname: nickname.into_active_value(),
+            password: password.into_active_value(),
             ..Default::default()
         };
 
-        let created_user = new_user.insert(&self.sea_service.db).await.unwrap();
+        let db = &self.sea_service.db;
+
+        let created_user = new_user.insert(db).await.unwrap();
         Ok(created_user)
     }
 
-    async fn find_all(&self) -> Result<Vec<entities::user::Model>, sea_orm::DbErr> {
+    pub async fn find_all(&self) -> Result<Vec<UserModel>, sea_orm::DbErr> {
         entities::user::Entity::find().all(&self.sea_service.db).await
+    }
+
+    pub async fn find_by_nickname(&self, nickname: &String) -> Result<std::option::Option<entities::user::Model>, sea_orm::DbErr> {
+        entities::user::Entity::find().filter(entities::user::Column::Nickname.eq(nickname)).one(&self.sea_service.db).await
     }
 }
