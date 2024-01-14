d32bb71422a44893cdf369c3a51e6d2df2ca2f86
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-14 00:21:32 -0300

    chore: delete repositories traits - since async functions can't let traits be object safe traits, this is going to be useless

--- src/repositories/article_repository.rs
+++
@@ -1,7 +0,0 @@
-use std::future::Future;
-
-pub trait ArticleRepository {
-    // async fn find_by_id(&self, id: String) -> Result<Option<entities::article::Model>, sea_orm::DbErr>;
-    fn find_by_id(&self, id: String) -> impl Future<Output = Result<Option<entities::article::Model>, sea_orm::DbErr>> + Send;
-    fn find_all(&self) -> impl Future<Output = Result<Vec<entities::article::Model>, sea_orm::DbErr>>;
-}
