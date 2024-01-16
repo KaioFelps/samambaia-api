9b0cba06591416cbf300e3ce88e403248908f92f
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-15 23:49:56 -0300

    chore: create user service - change database error by internal server error

--- src/services/create_user_service.rs
+++ src/services/create_user_service.rs
@@ -1,6 +1,7 @@
 use entities::user::Model as UserModel;
+use crate::errors::internal_error::InternalError;
 use crate::infra::sea::repositories::sea_user_repository::SeaUserRepository;
-use crate::errors::{user_already_exists_error::UserAlreadyExistsError, database_error::DatabaseError};
+use crate::errors::user_already_exists_error::UserAlreadyExistsError;
 use password_auth::generate_hash;
 use entities::sea_orm_active_enums::Role as UserRole;
 
@@ -13,9 +14,9 @@ pub struct CreateUserService {
 }
 
 #[derive(Debug)]
-pub enum CreateUserServiceErrors<UserExist, DB> {
+pub enum CreateUserServiceErrors<UserExist, Internal> {
     UserAlreadyExist(UserExist),
-    DatabaseConn(DB)
+    InternalError(Internal)
 }
 
 impl CreateUserService {
@@ -26,18 +27,18 @@ impl CreateUserService {
     }
 
     pub async fn exec(&self, params: CreateUserParams)
-    -> Result<UserModel, CreateUserServiceErrors<UserAlreadyExistsError, DatabaseError>>
+    -> Result<UserModel, CreateUserServiceErrors<UserAlreadyExistsError, InternalError>>
     { self.create(params, UserRole::User).await }
 
     pub async fn exec_with_custom_role(&self, params: CreateUserParams, role: UserRole)
-    -> Result<UserModel, CreateUserServiceErrors<UserAlreadyExistsError, DatabaseError>>
+    -> Result<UserModel, CreateUserServiceErrors<UserAlreadyExistsError, InternalError>>
     { self.create(params, role).await }
 
-    async fn create(&self, params: CreateUserParams, role: UserRole) -> Result<UserModel, CreateUserServiceErrors<UserAlreadyExistsError, DatabaseError>> {
+    async fn create(&self, params: CreateUserParams, role: UserRole) -> Result<UserModel, CreateUserServiceErrors<UserAlreadyExistsError, InternalError>> {
         let user_on_db = &self.users_repository.find_by_nickname(&params.nickname).await;
 
         if user_on_db.is_err() {
-            return Err(CreateUserServiceErrors::DatabaseConn(DatabaseError::new()));
+            return Err(CreateUserServiceErrors::InternalError(InternalError::new()));
         }
 
         if let Some(_user) = user_on_db.as_ref().unwrap() {
@@ -49,7 +50,7 @@ impl CreateUserService {
        let created_user = self.users_repository.create(params.nickname, hashed_password, role).await;
 
         if user_on_db.is_err() {
-            return Err(CreateUserServiceErrors::DatabaseConn(DatabaseError::new()));
+            return Err(CreateUserServiceErrors::InternalError(InternalError::new()));
         }
 
         let created_user = created_user.unwrap();
