5b07a1c73583d27fb4ccb1c545f713d3de540c22
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-18 19:36:57 -0300

    fix: factories - add new dependencies to factories

--- src/domain/factories/update_user_service_factory.rs
+++ src/domain/factories/update_user_service_factory.rs
@@ -1,3 +1,4 @@
+use crate::infra::cryptography::PasswordAuthHasherAndVerifier;
 use crate::infra::sea::sea_service::SeaService;
 use crate::infra::sea::repositories::sea_user_repository::SeaUserRepository;
 use crate::domain::services::update_user_service::UpdateUserService;
@@ -6,8 +7,10 @@ pub async fn exec() -> UpdateUserService<SeaUserRepository> {
     let sea_service = SeaService::new().await;
 
     let user_repository: Box<SeaUserRepository> = Box::new(SeaUserRepository::new(sea_service).await);
+
+    let hasher = Box::new(PasswordAuthHasherAndVerifier {});
     
-    let update_user_service = UpdateUserService::new(user_repository);
+    let update_user_service = UpdateUserService::new(user_repository, hasher);
 
     update_user_service
 }
