bc76032ca7d7b503b0f848ffacbac1434d4c68b8
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-18 19:29:37 -0300

    feat: add domain entities

---
+++ src/domain/domain_entities/user.rs
@@ -0,0 +1,99 @@
+use chrono::{NaiveDateTime as DateTime, Utc};
+use uuid::Uuid;
+use super::role::Role;
+
+#[derive(Clone)]
+pub struct User {
+    id: Uuid,
+    nickname: String,
+    password: String,
+    created_at: DateTime,
+    last_login: Option<DateTime>,
+    role: Option<Role>,
+}
+
+impl User {
+    
+    // CONSTRUCTOR
+    pub fn new(
+        nickname: String,
+        password: String,
+        role: Option<Role>
+    ) -> Self {
+        let id = Uuid::new_v4();
+
+        let created_at  = Utc::now().naive_utc();
+        let last_login = None;
+
+        User {
+            id,
+            nickname,
+            password,
+            created_at,
+            last_login,
+            role
+        }
+    }
+
+    pub fn new_from_existing(
+        id: Uuid,
+        nickname: String,
+        password: String,
+        created_at: DateTime,
+        last_login: Option<DateTime>,
+        role: Option<Role>,
+    ) -> Self {
+        User {
+            id,
+            nickname,
+            password,
+            created_at,
+            last_login,
+            role
+        }
+    }
+    
+
+    // GETTERS
+    pub fn id(&self) -> Uuid {
+        self.id
+    }
+
+    pub fn nickname(&self) -> &str {
+        self.nickname.as_ref()
+    }
+
+    pub fn password(&self) -> &str {
+        self.password.as_ref()
+    }
+
+    pub fn created_at(&self) -> DateTime {
+        self.created_at
+    }
+
+    pub fn last_login(&self) -> Option<DateTime> {
+        self.last_login
+    }
+
+    pub fn role(&self) -> Option<Role> {
+        self.role.clone()
+    }
+
+    // SETTERS
+
+    pub fn set_nickname(&mut self, nickname: String) {
+        self.nickname = nickname;
+    }
+
+    pub fn set_last_login(&mut self, last_login: Option<DateTime>) {
+        self.last_login = last_login;
+    }
+
+    pub fn set_role(&mut self, role: Option<Role>) {
+        self.role = role;
+    }
+
+    pub fn set_password(&mut self, password: String) {
+        self.password = password;
+    }
+}
