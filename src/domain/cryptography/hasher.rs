ca7f5b92066e9a33fceb9e478aaaa41f16e0af2e
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-21 01:38:23 -0300

    chore: provide mocked struct to hasher

--- src/domain/cryptography/hasher.rs
+++ src/domain/cryptography/hasher.rs
@@ -1,3 +1,10 @@
+use async_trait::async_trait;
+
+#[cfg(test)]
+use mockall::automock;
+
+#[cfg_attr(test, automock)]
+#[async_trait]
 pub trait HasherTrait {
     fn hash(&self, password: String) -> String;
 }
