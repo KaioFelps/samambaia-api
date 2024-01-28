f40d2dcfe6227ab9fca2f14b1d1d486669672a5a
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-28 17:27:11 -0300

    feat: find_by_id and save methods to article repository

--- src/domain/repositories/article_repositoy.rs
+++ src/domain/repositories/article_repositoy.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use uuid::Uuid;
 use std::error::Error;
 
 use crate::domain::domain_entities::article::Article;
@@ -11,6 +12,11 @@ use mockall::automock;
 #[async_trait]
 pub trait ArticleRepositoryTrait {
     async fn create(&self, article: Article) -> Result<Article, Box<dyn Error>>;
+
+    async fn find_by_id(&self, id: Uuid) -> Result<Option<Article>, Box<dyn Error>>;
+
+    async fn save(&self, article: Article) -> Result<Article, Box<dyn Error>>;
+
 }
 
 #[cfg(test)]
--- src/infra/sea/repositories/sea_article_repository.rs
+++ src/infra/sea/repositories/sea_article_repository.rs
@@ -1,11 +1,16 @@
 use async_trait::async_trait;
-use sea_orm::ActiveModelTrait;
+use sea_orm::ColumnTrait;
+use sea_orm::{ActiveModelTrait, EntityTrait, QueryFilter};
+use uuid::Uuid;
 use std::error::Error;
 
 use crate::domain::repositories::article_repositoy::ArticleRepositoryTrait;
 use crate::domain::domain_entities::article::Article;
 use crate::infra::sea::mappers::sea_article_mapper::SeaArticleMapper;
 use crate::infra::sea::sea_service::SeaService;
+
+use entities::article::Entity as ArticleEntity;
+use entities::article::Column as ArticleColumn;
 
 pub struct SeaArticleRepository {
     sea_service: SeaService
@@ -31,6 +36,40 @@ impl ArticleRepositoryTrait for SeaArticleRepository {
         let created_article = SeaArticleMapper::model_to_article(created_article);
 
         Ok(created_article)
+    }
+
+    async fn find_by_id(&self, id: Uuid) -> Result<Option<Article>, Box<dyn Error>> {
+        let article = ArticleEntity::find_by_id(id).one(&self.sea_service.db).await;
+
+        match article {
+            Ok(article) => {
+                if article.is_none() {
+                    return Ok(None);
+                }
+
+                let mapped_article = SeaArticleMapper::model_to_article(article.unwrap());
+
+
+                Ok(Some(mapped_article))
+            },
+            Err(err) => Err(Box::new(err))
+        }
+    }
+
+    async fn save(&self, article: Article) -> Result<Article, Box<dyn Error>> {
+        let article_id = &article.id().clone();
+
+        let article = SeaArticleMapper::article_to_sea_active_model(article);
+
+        let res = ArticleEntity::update(article.clone())
+        .filter(ArticleColumn::Id.eq(*article_id))
+        .exec(&self.sea_service.db)
+        .await;
+
+        match res {
+            Ok(article) => Ok(SeaArticleMapper::model_to_article(article)),
+            Err(err) => Err(Box::new(err))
+        }
     }
 }
 
