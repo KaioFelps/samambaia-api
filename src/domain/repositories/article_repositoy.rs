7bcb11c6a622d399dc8f55924f4c4c0553a8a13d
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-01-28 03:40:47 -0300

    feat: create method to article repository

--- src/domain/repositories/article_repositoy.rs
+++ src/domain/repositories/article_repositoy.rs
@@ -1,14 +1,46 @@
 use async_trait::async_trait;
+use std::error::Error;
+
+use crate::domain::domain_entities::article::Article;
 
 #[cfg(test)]
 use mockall::automock;
 
+
 #[cfg_attr(test, automock)]
 #[async_trait]
 pub trait ArticleRepositoryTrait {
+    async fn create(&self, article: Article) -> Result<Article, Box<dyn Error>>;
 }
 
 #[cfg(test)]
 mod test {
+    use tokio;
+    use uuid::Uuid;
+    use super::*;
 
+    #[tokio::test]
+    async fn create() {
+        let mut db: Vec<Article> = vec![];
+        let mut mocked_repo = MockArticleRepositoryTrait::default();
+
+        mocked_repo
+        .expect_create()
+        .returning(move |article: Article| {
+            db.push(article);
+
+            Ok(db[0].clone())
+        });
+
+        let article = Article::new(
+            Uuid::new_v4(),
+            "Title".to_string(),
+            "Content".to_string(),
+            "cover-url.com".to_string()
+        );
+
+        let result = mocked_repo.create(article).await;
+
+        assert!(!result.unwrap().id().is_nil());
+    }
 }
--- src/infra/sea/repositories/sea_article_repository.rs
+++ src/infra/sea/repositories/sea_article_repository.rs
@@ -1,5 +1,11 @@
 use async_trait::async_trait;
-use crate::{domain::repositories::article_repositoy::ArticleRepositoryTrait, infra::sea::sea_service::SeaService};
+use sea_orm::ActiveModelTrait;
+use std::error::Error;
+
+use crate::domain::repositories::article_repositoy::ArticleRepositoryTrait;
+use crate::domain::domain_entities::article::Article;
+use crate::infra::sea::mappers::sea_article_mapper::SeaArticleMapper;
+use crate::infra::sea::sea_service::SeaService;
 
 pub struct SeaArticleRepository {
     sea_service: SeaService
@@ -7,7 +13,7 @@ pub struct SeaArticleRepository {
 
 impl SeaArticleRepository {
     // constructor
-    pub async fn new(service: &SeaService) -> Self {
+    pub async fn new(service: SeaService) -> Self {
         SeaArticleRepository {
             sea_service: service.clone()
         }
@@ -16,5 +22,15 @@ impl SeaArticleRepository {
 
 #[async_trait]
 impl ArticleRepositoryTrait for SeaArticleRepository {
+    async fn create(&self, article: Article) -> Result<Article, Box<dyn Error>> {
+        let new_article = SeaArticleMapper::article_to_sea_active_model(article);
+
+        let db = &self.sea_service.db;
+
+        let created_article = new_article.insert(db).await.unwrap();
+        let created_article = SeaArticleMapper::model_to_article(created_article);
+
+        Ok(created_article)
+    }
 }
 
