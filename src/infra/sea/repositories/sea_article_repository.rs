5fe618ed9e3fdaa2c3407ff9f1fa52c2ba5c4a2c
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-10 13:54:53 -0300

    chore: repositories errors - propagate repositories errors up instead of matching and re-returning the error

--- src/infra/sea/repositories/sea_article_repository.rs
+++ src/infra/sea/repositories/sea_article_repository.rs
@@ -8,7 +8,6 @@ use std::error::Error;
 use crate::core::pagination::{PaginationParameters, Query, QueryType};
 use crate::domain::repositories::article_repository::{ArticleRepositoryTrait, FindManyResponse};
 use crate::domain::domain_entities::article::Article;
-use crate::errors::internal_error::InternalError;
 use crate::infra::sea::mappers::sea_article_mapper::SeaArticleMapper;
 use crate::infra::sea::sea_service::SeaService;
 
@@ -35,36 +34,27 @@ impl ArticleRepositoryTrait for SeaArticleRepository {
 
         let db = &self.sea_service.db;
 
-        let created_article = new_article.insert(db).await.unwrap();
+        let created_article = new_article.insert(db).await?;
         let created_article = SeaArticleMapper::model_to_article(created_article);
 
         Ok(created_article)
     }
 
     async fn find_by_id(&self, id: Uuid) -> Result<Option<Article>, Box<dyn Error>> {
-        let article = ArticleEntity::find_by_id(id).one(&self.sea_service.db).await;
+        let article = ArticleEntity::find_by_id(id).one(&self.sea_service.db).await?;
 
-        match article {
-            Ok(article) => {
-                if article.is_none() {
-                    return Ok(None);
-                }
-
-                let mapped_article = SeaArticleMapper::model_to_article(article.unwrap());
-
-
-                Ok(Some(mapped_article))
-            },
-            Err(err) => Err(Box::new(err))
+        if article.is_none() {
+            return Ok(None);
         }
+
+        let mapped_article = SeaArticleMapper::model_to_article(article.unwrap());
+
+        Ok(Some(mapped_article))
     }
 
     async fn find_many(&self, params: PaginationParameters) -> Result<FindManyResponse, Box<dyn Error>> {
         #[allow(unused_mut)]
         let mut articles_response;
-
-        #[allow(unused_mut)]
-        let mut articles_count_response;
 
         let current_page = params.page as u64;
         let items_per_page = params.items_per_page as u64;
@@ -76,25 +66,18 @@ impl ArticleRepositoryTrait for SeaArticleRepository {
         .apply_if(params.clone().query, |#[allow(unused_mut)] mut query_builder, query| self.find_many_get_filters(query_builder, query))
         .limit(items_per_page)
         .offset(leap)
-        .all(&self.sea_service.db).await;
+        .all(&self.sea_service.db).await?;
 
-        articles_count_response = ArticleEntity::find()
+        let articles_count = ArticleEntity::find()
         .apply_if(params.query, |#[allow(unused_mut)] mut query_builder, query| self.find_many_get_filters(query_builder, query))
         .offset(leap)
-        .count(&SeaService::new().await.db).await;
-
-
-        if articles_response.is_err() || articles_count_response.is_err() {
-            return Err(Box::new(InternalError::new()));
-        }
+        .count(&SeaService::new().await.db).await?;
 
         let mut articles: Vec<Article> = vec![];
 
-        for article in articles_response.unwrap().into_iter() {
+        for article in articles_response.into_iter() {
             articles.push(SeaArticleMapper::model_to_article(article));
         }
-
-        let articles_count = articles_count_response.unwrap();
 
         Ok(FindManyResponse(articles, articles_count))
     }
@@ -104,13 +87,7 @@ impl ArticleRepositoryTrait for SeaArticleRepository {
         .limit(3)
         .order_by_desc(ArticleColumn::CreatedAt)
         .all(&self.sea_service.db)
-        .await;
-
-        if articles.is_err() {
-            return Err(Box::new(articles.unwrap_err()));
-        }
-
-        let articles = articles.unwrap();
+        .await?;
 
         let mut mapped_articles: Vec<Article> = vec![];
 
@@ -128,28 +105,22 @@ impl ArticleRepositoryTrait for SeaArticleRepository {
 
         let article = SeaArticleMapper::article_to_sea_active_model(article);
 
-        let res = ArticleEntity::update(article.clone())
+        let article = ArticleEntity::update(article.clone())
         .filter(ArticleColumn::Id.eq(*article_id))
         .exec(&self.sea_service.db)
-        .await;
+        .await?;
 
-        match res {
-            Ok(article) => Ok(SeaArticleMapper::model_to_article(article)),
-            Err(err) => Err(Box::new(err))
-        }
+        Ok(SeaArticleMapper::model_to_article(article))
     }
 
     async fn delete(&self, article: Article) -> Result<(), Box<dyn Error>> {
         let article = SeaArticleMapper::article_to_sea_active_model(article);
 
-        let res = ArticleEntity::delete(article)
+        ArticleEntity::delete(article)
         .exec(&self.sea_service.db)
-        .await;
+        .await?;
 
-        match res {
-            Ok(_) => Ok(()),
-            Err(err) => Err(Box::new(err))
-        }
+        Ok(())
     }
 }
 
--- src/infra/sea/repositories/sea_user_repository.rs
+++ src/infra/sea/repositories/sea_user_repository.rs
@@ -29,40 +29,35 @@ impl UserRepositoryTrait for SeaUserRepository {
 
         let db = &self.sea_service.db;
 
-        let created_user = new_user.insert(db).await.unwrap();
+        let created_user = new_user.insert(db).await?;
         let created_user = SeaUserMapper::model_to_user(created_user);
 
         Ok(created_user)
     }
 
     async fn find_by_nickname(&self, nickname: &String) -> Result<Option<User>, Box<dyn Error>> {
-        let user = UserEntity::find().filter(UserColumn::Nickname.eq(nickname)).one(&self.sea_service.db).await;
+        let user = UserEntity::find()
+        .filter(UserColumn::Nickname.eq(nickname))
+        .one(&self.sea_service.db)
+        .await?;
 
-        match user {
-            Ok(user) => {
-                if user.is_none() {
-                    return Ok(None);
-                }
-
-                return Ok(Some(SeaUserMapper::model_to_user(user.unwrap())));
-            },
-            Err(err) => Err(Box::new(err))
+        if user.is_none() {
+            return Ok(None);
         }
+
+        return Ok(Some(SeaUserMapper::model_to_user(user.unwrap())));
     }
 
     async fn find_by_id(&self, id: &Uuid) -> Result<Option<User>, Box<dyn Error>> {
-        let user = UserEntity::find_by_id(*id).one(&self.sea_service.db).await;
+        let user = UserEntity::find_by_id(*id)
+        .one(&self.sea_service.db)
+        .await?;
 
-        match user {
-            Ok(user) => {
-                if user.is_none() {
-                    return Ok(None);
-                }
-
-                return Ok(Some(SeaUserMapper::model_to_user(user.unwrap())));
-            },
-            Err(err) => Err(Box::new(err))
+        if user.is_none() {
+            return Ok(None);
         }
+
+        return Ok(Some(SeaUserMapper::model_to_user(user.unwrap())));
     }
 
     async fn save(&self, user: User) -> Result<User, Box<dyn Error>> {
@@ -70,11 +65,13 @@ impl UserRepositoryTrait for SeaUserRepository {
 
         let user = SeaUserMapper::user_to_sea_active_model(user.clone());
 
-        let res = UserEntity::update(user.clone()).filter(UserColumn::Id.eq(*user_id)).exec(&self.sea_service.db).await;
+        let user = UserEntity::update(user.clone())
+        .filter(UserColumn::Id.eq(*user_id))
+        .exec(&self.sea_service.db)
+        .await?;
 
-        match res {
-            Ok(model) => return Ok(SeaUserMapper::model_to_user(model)),
-            Err(err) => return Err(Box::new(err))
-        }
+        let user = SeaUserMapper::model_to_user(user);
+
+        Ok(user)
     }
 }
