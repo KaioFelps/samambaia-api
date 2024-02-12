a1ea9e7c58ffd9cacd9d5b0cb5cbeea0802a1a9b
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-12 04:07:21 -0300

    chore: clean main

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,21 @@
 use dotenvy::dotenv;
-use entities::user::Column;
-// use hubbitos_backend::domain::services::create_article_service::CreateArticleParams;
-// use hubbitos_backend::domain::services::fetch_many_articles_service::FetchManyArticlesParams;
-
-
-use hubbitos_backend::domain::domain_entities::comment::Comment;
-use hubbitos_backend::domain::factories::{create_article_service_factory, create_user_service_factory, delete_comment_service_factory};
-use hubbitos_backend::domain::services::create_user_service::CreateUserParams;
-use hubbitos_backend::domain::domain_entities::role::Role;
-use hubbitos_backend::domain::services::delete_comment_service::DeleteCommentParams;
-use hubbitos_backend::infra::sea::mappers::sea_user_mapper::SeaUserMapper;
-use hubbitos_backend::infra::sea::sea_service::SeaService;
-use migration::{IntoCondition, IntoTableRef};
-use sea_orm::{ColumnTrait, Condition, ConnectionTrait, EntityTrait, QueryFilter, QuerySelect, QueryTrait, RelationTrait};
-use uuid::uuid;
-
 use env_logger::{self, Target};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 
-// use entities::article::Entity as ArticleEntity;
-// use entities::article::Column as ArticleColumn;
-
+use hubbitos_backend::infra::sea::sea_service::SeaService;
+use hubbitos_backend::infra::sea::mappers::sea_user_mapper::SeaUserMapper;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     env_logger::Builder::new().parse_env("RUST_LOG").target(Target::Stdout).init();
 
-    let floricultor_user = entities::user::Entity::find().filter(Column::Nickname.eq("Floricultor".to_owned())).one(&SeaService::new().await.db).await.unwrap().unwrap();
+    let floricultor_user = entities::user::Entity::find()
+    .filter(entities::user::Column::Nickname.eq("Floricultor".to_owned()))
+    .one(&SeaService::new().await.db)
+    .await
+    .unwrap()
+    .unwrap();
+
     let _floricultor_user = SeaUserMapper::model_to_user(floricultor_user);
-    // let change_password_service = change_password_service_factory::exec().await;
-
-    let _create_article_service = create_article_service_factory::exec().await;
-
-    // let _ = create_article_service.exec(CreateArticleParams {
-    //     author_id: floricultor_user.id(),
-    //     content: "conteúdo da primeira notícia".to_string(),
-    //     cover_url: "url".to_string(),
-    //     title: "Primeira notícia".to_string()
-    // }).await;
-
-    // let _ = create_article_service.exec(CreateArticleParams {
-    //     author_id: floricultor_user.id(),
-    //     content: "conteúdo da segundaaaaa notícia".to_string(),
-    //     cover_url: "url".to_string(),
-    //     title: "SEGUNDA NOTICIAAA".to_string()
-    // }).await;
-
-    // let fetch_articles_service = fetch_many_articles_service_factory::exec().await;
-
-    // let articles = fetch_articles_service.exec(FetchManyArticlesParams {
-    //     page: None,
-    //     per_page: None,
-    //     query: None,
-    //     query_by: None
-    // }).await;
-
-    // println!("{:#?}", articles.unwrap());
-
-    // let cas = comment_on_article_service_factory::exec().await;
-
-    // let res = cas.exec(CommentOnArticleParams {
-    //     article_id: uuid!("a8df0e54-efd4-47cf-867a-7387cbd065ac"),
-    //     author_id: uuid!("eb2521c7-3ea5-45c1-8c24-dff8e0d0cc42"),
-    //     content: "comentando na primeira noticiaaaa".into()
-    // }).await;
-
-    // let dc = delete_comment_service_factory::exec().await;
-
-    // let _ = dc.exec(DeleteCommentParams {
-    //     user_id: _floricultor_user.id(),
-    //     comment_id: uuid!("f65d2a07-9f42-4754-b052-4272b1a27b22")
-    // }).await;
 }
