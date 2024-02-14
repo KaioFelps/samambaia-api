3adca40deba6693147873b0af3b7371db6e4654b
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-14 16:01:14 -0300

    feat: paginated fetch many comment service

---
+++ src/domain/services/fetch_many_comment_reports_service.rs
@@ -0,0 +1,235 @@
+use std::error::Error;
+use log::error;
+use crate::domain::domain_entities::comment_report::CommentReport;
+use crate::{R_EOL, LOG_SEP};
+
+use crate::core::pagination::{PaginationParameters, PaginationResponse, Query};
+use crate::domain::repositories::comment_report_repository::{CommentReportQueryType, CommentReportRepositoryTrait, FindManyCommentReportsResponse};
+use crate::errors::internal_error::InternalError;
+
+pub struct FetchManyCommentReportsParams {
+    pub page: Option<u32>,
+    pub per_page: Option<u32>,
+    pub query_string: Option<String>,
+    pub query_bool: Option<bool>,
+    pub query_by: Option<CommentReportQueryType>    
+}
+
+
+#[derive(Debug)]
+pub struct FetchManyCommentReportsResponse {
+    pub pagination: PaginationResponse,
+    pub data: Vec<CommentReport>
+}
+
+pub struct FetchManyCommentReportsService<CommentReportRepository: CommentReportRepositoryTrait> {
+    comment_report_repository: Box<CommentReportRepository>
+}
+
+impl<CommentReportRepository: CommentReportRepositoryTrait>
+FetchManyCommentReportsService<CommentReportRepository> {
+    pub fn new(
+        comment_report_repository: Box<CommentReportRepository>
+    ) -> Self {
+        FetchManyCommentReportsService {
+            comment_report_repository
+        }
+    }
+
+    pub async fn exec(&self, params: FetchManyCommentReportsParams) -> Result<FetchManyCommentReportsResponse, Box<dyn Error>> {
+        let default_items_per_page = 9;
+        let default_page = 1;
+
+        let items_per_page = if params.per_page.is_some() { params.per_page.unwrap() } else { default_items_per_page };
+        
+        let page = if params.page.is_some() {
+            let params_page = params.page.unwrap();
+            if params_page <= 0 { default_page } else { params_page }
+        } else { default_page };
+
+        let query = self.parse_query(
+            params.query_string,
+            params.query_bool,
+            params.query_by
+        ).await;
+
+        let response = self.comment_report_repository.find_many(
+            PaginationParameters {
+                items_per_page,
+                page,
+                query,
+            }
+        ).await;
+
+        if response.is_err() {
+            error!(
+                "{R_EOL}{LOG_SEP}{R_EOL}Error occurred on Fetch Many Articles Service, while finding many articles from database: {R_EOL}{}{R_EOL}{LOG_SEP}{R_EOL}",
+                response.as_ref().unwrap_err()
+            );
+
+            return Err(Box::new(InternalError::new()));
+        }
+
+        let response = response.unwrap();
+        let FindManyCommentReportsResponse (data, total_items) = response;
+
+        Ok(FetchManyCommentReportsResponse {
+            pagination: PaginationResponse {
+                total_items,
+                current_page: page,
+                total_pages: (total_items as f64 / items_per_page as f64).ceil() as u32,
+            },
+            data,
+        })
+    }
+
+    async fn parse_query(
+        &self,
+        query_string: Option<String>,
+        query_bool: Option<bool>,
+        query_by: Option<CommentReportQueryType>
+    ) -> Option<Query<CommentReportQueryType>> {
+        if query_string.is_none() && query_bool.is_none() {
+            return None;
+        }
+
+        match query_by.unwrap() {
+            CommentReportQueryType::CONTENT => {
+                Some(
+                    Query {
+                        content: query_string.unwrap(),
+                        query_type: CommentReportQueryType::CONTENT,
+                    }
+                )
+            },
+            CommentReportQueryType::SOLVED => {
+                let is_solved = if query_bool.unwrap() { "TRUE" } else { "FALSE" }.to_string();
+
+                Some(
+                    Query {
+                        content: is_solved,
+                        query_type: CommentReportQueryType::SOLVED,
+                    }
+                )
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::domain::{domain_entities::comment_report::CommentReportTrait, repositories::comment_report_repository::MockCommentReportRepositoryTrait};
+
+    use super::*;
+
+    use std::sync::{Arc, Mutex};
+
+    use tokio;
+    use uuid::Uuid;
+    use chrono::Utc;
+
+    #[tokio::test]
+    async fn test() {
+        let comment_report_db: Arc<Mutex<Vec<CommentReport>>> = Arc::new(Mutex::new(Vec::new()));
+
+        let comm_rep_1 = CommentReport::new_from_existing(
+            1,
+            Uuid::new_v4(),
+            Uuid::new_v4(),
+            "report numero 1".into(),
+            false,
+            Utc::now().naive_utc()
+        );
+
+        let comm_rep_2 = CommentReport::new_from_existing(
+            2,
+            Uuid::new_v4(),
+            Uuid::new_v4(),
+            "report numero 2".into(),
+            true,
+            Utc::now().naive_utc()
+        );
+
+        comment_report_db.lock().unwrap().push(comm_rep_1);
+        comment_report_db.lock().unwrap().push(comm_rep_2);
+
+        let mut mocked_comm_report_repo = MockCommentReportRepositoryTrait::new();
+
+        let comm_repo_db_clone = Arc::clone(&comment_report_db);
+        mocked_comm_report_repo
+        .expect_find_many()
+        .returning(move |params| {
+            let PaginationParameters {
+                items_per_page,
+                page,
+                query
+            } = params;
+
+            let mut comment_reports: Vec<CommentReport> = vec![];
+
+            if query.is_some() {
+                let query = query.unwrap();
+
+                let content = query.content;
+                let query_type = query.query_type;
+
+                for item in comm_repo_db_clone.lock().unwrap().iter() {
+                    match query_type {
+                        CommentReportQueryType::CONTENT => {
+                            if item.message().contains(&content[..]) {
+                                comment_reports.push(item.clone());
+                            }
+                        },
+                        CommentReportQueryType::SOLVED => {
+                            let is_solved = {
+                                if content == "TRUE" {
+                                    true
+                                } else {
+                                    false
+                                }
+                            };
+
+                            if item.solved().eq(&is_solved) {
+                                comment_reports.push(item.clone());
+                            }
+                        }
+                    };
+                }
+            } else {
+                comment_reports = comm_repo_db_clone.lock().unwrap().to_vec();
+            }
+
+            let total_of_items_before_paginating = comment_reports.len();
+
+            let leap = (page - 1) * items_per_page;
+
+            let mut res_comment_reports = vec![];
+
+            for (index, item) in comment_reports.iter().enumerate() {
+                if index >= leap as usize {
+                    res_comment_reports.push(item.to_owned());
+                }
+            }
+
+            Ok(FindManyCommentReportsResponse (res_comment_reports, total_of_items_before_paginating as u64))
+        });
+
+        let sut = FetchManyCommentReportsService {
+            comment_report_repository: Box::new(mocked_comm_report_repo)
+        };
+
+        let res = sut.exec(FetchManyCommentReportsParams {
+            page: Some(1),
+            per_page: Some(1),
+            query_bool: Some(true),
+            query_by: Some(CommentReportQueryType::SOLVED),
+            query_string: None
+        }).await;
+
+        let res = res.unwrap();
+
+        assert_eq!("report numero 2".to_string(), res.data[0].message());
+        assert_eq!(1, res.pagination.total_pages);
+        assert_eq!(1, res.pagination.total_items);
+    }
+}
--- src/domain/services/mod.rs
+++ src/domain/services/mod.rs
@@ -16,3 +16,4 @@ pub mod fetch_many_comments_service;
 pub mod create_comment_report_service;
 pub mod solve_comment_report_service;
 pub mod delete_comment_report_service;
+pub mod fetch_many_comment_reports_service;
