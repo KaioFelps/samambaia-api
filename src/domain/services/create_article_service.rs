aa4d3cc67a77bbf3dc713fab273af40f13918fca
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-27 20:09:43 -0300

    feat: create article service + factory

--- src/domain/services/create_article_service.rs
+++ src/domain/services/create_article_service.rs
@@ -2,9 +2,9 @@ use std::error::Error;
 use log::error;
 use uuid::Uuid;
 
-use crate::domain::domain_entities::{article::Article, role::Role};
-use crate::domain::repositories::{article_repository::ArticleRepositoryTrait, user_repository::UserRepositoryTrait};
-use crate::errors::{internal_error::InternalError, unauthorized_error::UnauthorizedError};
+use crate::domain::domain_entities::article::Article;
+use crate::domain::repositories::article_repository::ArticleRepositoryTrait;
+use crate::errors::internal_error::InternalError;
 use crate::{LOG_SEP, R_EOL};
 
 pub struct CreateArticleParams {
@@ -15,46 +15,19 @@ pub struct CreateArticleParams {
 }
 pub struct CreateArticleService<
 ArticleRepository: ArticleRepositoryTrait,
-UserRepository: UserRepositoryTrait
 > {
-    user_repository: Box<UserRepository>,
     article_repository: Box<ArticleRepository>,
 }
 
-impl
-<ArticleRepository: ArticleRepositoryTrait,
-UserRepository: UserRepositoryTrait>
-CreateArticleService<ArticleRepository, UserRepository>
+impl<ArticleRepository: ArticleRepositoryTrait> CreateArticleService<ArticleRepository>
 {
-    pub fn new(article_repository: Box<ArticleRepository>, user_repository: Box<UserRepository>) -> Self {
+    pub fn new(article_repository: Box<ArticleRepository>) -> Self {
         CreateArticleService {
             article_repository,
-            user_repository,
         }
     }
 
     pub async fn exec(&self, params: CreateArticleParams) -> Result<Article, Box<dyn Error>> {
-        let user_on_db = &self.user_repository.find_by_id(&params.author_id).await;
-
-        if user_on_db.is_err() {
-            error!(
-                "{R_EOL}{LOG_SEP}{R_EOL}Error occurred on Create Article Service, while fetching user from database:{R_EOL}{}{R_EOL}{LOG_SEP}{R_EOL}",
-                user_on_db.as_ref().unwrap_err()
-            );
-
-            return Err(
-                Box::new(InternalError::new())
-            );
-        }
-
-        let user_on_db = user_on_db.as_ref().unwrap().to_owned();
-
-        if user_on_db.is_none() || user_on_db.unwrap().role().unwrap() == Role::User {
-            return Err(
-                Box::new(UnauthorizedError::new())
-            )
-        }
-
         let article = Article::new(
             params.author_id,
             params.title,
@@ -81,33 +54,14 @@ CreateArticleService<ArticleRepository, UserRepository>
 mod test {
     use uuid::Uuid;
 
-    use crate::domain::repositories::user_repository::MockUserRepositoryTrait;
     use crate::domain::repositories::article_repository::MockArticleRepositoryTrait;
-    use crate::domain::domain_entities::user::User;
-    use crate::domain::domain_entities::role::Role;
     use super::{Article, CreateArticleParams};
 
     #[tokio::test]
     async fn test() {
-        let mut mocked_user_repo: MockUserRepositoryTrait = MockUserRepositoryTrait::new();
         let mut mocked_article_repo: MockArticleRepositoryTrait = MockArticleRepositoryTrait::new();
 
         let mut db: Vec<Article> = vec![];
-
-        mocked_user_repo
-        .expect_find_by_id()
-        .returning(|id| {
-            let fake_user = User::new_from_existing(
-                id.clone().to_owned(),
-                "Fake name".to_string(),
-                "password".to_string(),
-                chrono::Utc::now().naive_utc(),
-                None,
-                Some(Role::Writter)
-            );
-
-            Ok(Some(fake_user))
-        });
 
         mocked_article_repo
         .expect_create()
@@ -119,7 +73,6 @@ mod test {
         .times(1);
 
         let service = super::CreateArticleService {
-            user_repository: Box::new(mocked_user_repo),
             article_repository: Box::new(mocked_article_repo)
         };
 
