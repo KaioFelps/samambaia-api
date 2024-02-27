44d663760c3edf913cb5b033aae75a80e7a90190
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-27 20:10:55 -0300

    feat: toggle comment visibility service + factory

--- src/domain/factories/toggle_comment_visibility_service_factory.rs
+++ src/domain/factories/toggle_comment_visibility_service_factory.rs
@@ -1,16 +1,13 @@
 use crate::infra::sea::sea_service::SeaService;
 use crate::domain::services::toggle_comment_visibility_service::ToggleCommentVisibilityService;
 use crate::infra::sea::repositories::sea_comment_repository::SeaCommentRepository;
-use crate::infra::sea::repositories::sea_user_repository::SeaUserRepository;
 
-pub async fn exec() -> ToggleCommentVisibilityService<SeaUserRepository, SeaCommentRepository> {
+pub async fn exec() -> ToggleCommentVisibilityService<SeaCommentRepository> {
     let sea_service = SeaService::new().await;
     
-    let user_repository: Box<SeaUserRepository> = Box::new(SeaUserRepository::new(sea_service.clone()).await);
     let comment_repository: Box<SeaCommentRepository> = Box::new(SeaCommentRepository::new(sea_service).await);
     
     let toggle_comment_visibility_service = ToggleCommentVisibilityService::new(
-        user_repository,
         comment_repository
     );
 
