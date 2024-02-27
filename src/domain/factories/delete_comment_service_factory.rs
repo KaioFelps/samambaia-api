72c8a0d931b61852d71e1b5b7fbb056ededc1c12
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-27 20:10:34 -0300

    feat: delete comment service + factory

--- src/domain/factories/delete_comment_service_factory.rs
+++ src/domain/factories/delete_comment_service_factory.rs
@@ -1,15 +1,13 @@
 use crate::domain::services::delete_comment_service::DeleteCommentService;
 use crate::infra::sea::repositories::sea_comment_repository::SeaCommentRepository;
 use crate::infra::sea::sea_service::SeaService;
-use crate::infra::sea::repositories::sea_user_repository::SeaUserRepository;
 
-pub async fn exec() -> DeleteCommentService<SeaCommentRepository, SeaUserRepository> {
+pub async fn exec() -> DeleteCommentService<SeaCommentRepository> {
     let sea_service = SeaService::new().await;
 
-    let user_repository: Box<SeaUserRepository> = Box::new(SeaUserRepository::new(sea_service.clone()).await);
     let comment_repository: Box<SeaCommentRepository> = Box::new(SeaCommentRepository::new(sea_service).await);
     
-    let delete_comment_service = DeleteCommentService::new(comment_repository, user_repository);
+    let delete_comment_service = DeleteCommentService::new(comment_repository);
 
     delete_comment_service
 }
