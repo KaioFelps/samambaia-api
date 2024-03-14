05611c55e258435a45ccbe0472f8583c9cbcf535
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-14 01:17:59 -0300

    feat: team role repository - find many response (trait) - query type (trait) - find many method interface (trait) - find many method implementation (sea repository)

--- src/domain/repositories/team_role_repository.rs
+++ src/domain/repositories/team_role_repository.rs
@@ -5,7 +5,18 @@ use std::error::Error;
 #[cfg(test)]
 use mockall::automock;
 
-use crate::domain::domain_entities::team_role::TeamRole;
+use crate::{core::pagination::PaginationParameters, domain::domain_entities::team_role::TeamRole};
+
+#[derive(Debug)]
+pub struct FindManyTeamRolesResponse (
+    pub Vec<TeamRole>,
+    pub u64,
+);
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum TeamRoleQueryType {
+    TITLE(String),
+}
 
 #[cfg_attr(test, automock)]
 #[async_trait]
@@ -17,4 +28,6 @@ pub trait TeamRoleRepositoryTrait {
     async fn save(&self, team_role: TeamRole) -> Result<TeamRole, Box<dyn Error>>;
 
     async fn delete(&self, team_role: TeamRole) -> Result<(), Box<dyn Error>>;
+
+    async fn find_many(&self, params: PaginationParameters<TeamRoleQueryType>) -> Result<FindManyTeamRolesResponse, Box<dyn Error>>;
 }
--- src/infra/sea/repositories/sea_team_role_repository.rs
+++ src/infra/sea/repositories/sea_team_role_repository.rs
@@ -1,15 +1,18 @@
 use std::error::Error;
 
 use async_trait::async_trait;
-use sea_orm::{ActiveModelTrait, EntityTrait};
+use migration::{Expr, Func};
+use sea_orm::{ActiveModelTrait, EntityTrait, PaginatorTrait, QueryFilter, QueryOrder, QuerySelect, QueryTrait};
 use uuid::Uuid;
 
+use crate::core::pagination::PaginationParameters;
 use crate::domain::domain_entities::team_role::TeamRole;
-use crate::domain::repositories::team_role_repository::TeamRoleRepositoryTrait;
+use crate::domain::repositories::team_role_repository::{FindManyTeamRolesResponse, TeamRoleQueryType, TeamRoleRepositoryTrait};
 use crate::infra::sea::mappers::sea_team_role_mapper::SeaTeamRoleMapper;
 use crate::infra::sea::sea_service::SeaService;
 
 use entities::team_role::Entity as TeamRoleEntity;
+use entities::team_role::Column as TeamRoleColumn;
 
 pub struct SeaTeamRoleRepository {
     sea_service: SeaService
@@ -60,5 +63,45 @@ impl TeamRoleRepositoryTrait for SeaTeamRoleRepository {
         team_role.delete(&self.sea_service.db).await?;
 
         Ok(())
+    }
+
+    async fn find_many(&self, params: PaginationParameters<TeamRoleQueryType>) -> Result<FindManyTeamRolesResponse, Box<dyn Error>> {
+        let team_roles_response;
+
+        let current_page = params.page as u64;
+        let items_per_page = params.items_per_page as u64;
+
+        let leap = ((&current_page - 1) * items_per_page) as u64;
+
+        team_roles_response = TeamRoleEntity::find()
+        .order_by_desc(TeamRoleColumn::CreatedAt)
+        .apply_if(params.clone().query, |query_builder, query| self.find_many_get_filters(query_builder, query))
+        .limit(items_per_page)
+        .offset(leap)
+        .all(&self.sea_service.db).await?;
+
+        let team_roles_count = TeamRoleEntity::find()
+        .apply_if(params.query, |query_builder, query| self.find_many_get_filters(query_builder, query))
+        .offset(leap)
+        .count(&self.sea_service.db).await?;
+
+        let mut team_roles: Vec<TeamRole> = vec![];
+
+        for team_role in team_roles_response.into_iter() {
+            team_roles.push(SeaTeamRoleMapper::model_to_team_role(team_role));
+        }
+
+        Ok(FindManyTeamRolesResponse(team_roles, team_roles_count))
+    }
+}
+
+impl SeaTeamRoleRepository {
+    fn find_many_get_filters(&self, #[allow(unused_mut)] mut query_builder: sea_orm::Select<TeamRoleEntity>, query: TeamRoleQueryType) -> sea_orm::Select<TeamRoleEntity> {
+        match query {
+            TeamRoleQueryType::TITLE(content) => {
+                let filter = Expr::expr(Func::lower(Expr::col(TeamRoleColumn::Title))).like(format!("%{}%", content));
+                query_builder.filter(filter.clone())
+            }
+        }
     }
 }
