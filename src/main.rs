f6ffe8ae8e63f3e76d2ca84eb2a900212764ff3a
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-20 00:10:59 -0300

    chore: clean main function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use actix_web::{middleware, App, HttpServer};
+use actix_web::HttpServer;
 use dotenvy::dotenv;
 use env_logger::{self, Target};
-use hubbitos_backend::infra::http::routes::api::ApiRoutes;
-
-use sea_orm::{EntityTrait, QueryFilter, ColumnTrait};
+use hubbitos_backend::server::ServerFactory;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -11,30 +9,9 @@ async fn main() -> std::io::Result<()> {
 
     env_logger::Builder::new().parse_env("RUST_LOG").target(Target::Stdout).init();
 
-    let _floricultor_user = hubbitos_backend::infra::sea::mappers::sea_user_mapper::SeaUserMapper::model_to_user(
-        entities::user::Entity::find()
-        .filter(entities::user::Column::Nickname.eq("Floricultor".to_owned()))
-        .one(&hubbitos_backend::infra::sea::sea_service::SeaService::new().await.db)
-        .await
-        .unwrap()
-        .unwrap()
-    );
-
-    let token = hubbitos_backend::infra::jwt::jwt_service::JwtService{}.make_jwt(
-        _floricultor_user.id(),
-        _floricultor_user.role().unwrap(),
-        jsonwebtoken::EncodingKey::from_secret(&hubbitos_backend::ENV_VARS.jwt_secret.as_ref())
-    ).unwrap();
-
-    println!("{:#?}", _floricultor_user);
-    println!("{:#?}", token);
-
-    HttpServer::new(|| {
-        App::new()
-        .wrap(middleware::NormalizePath::new(middleware::TrailingSlash::Trim))
-        .configure(ApiRoutes::register)
-    })
+    HttpServer::new(ServerFactory::exec)
     .bind(("127.0.0.1", 3000))?
     .run()
     .await
 }
+
