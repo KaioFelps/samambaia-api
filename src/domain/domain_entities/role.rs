4982d812edbf2dd580e45c5c4a9c09db591fbd3a
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-24 21:19:50 -0300

    feat: impl for_str to role enum

--- src/domain/domain_entities/role.rs
+++ src/domain/domain_entities/role.rs
@@ -1,4 +1,8 @@
+use std::str::FromStr;
+
 use serde::{Serialize, Deserialize};
+
+use crate::errors::enum_coercion_error::EnumCoercionError;
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Role {
@@ -9,4 +13,21 @@ pub enum Role {
     Principal,
     User,
     Writter,
+}
+
+impl FromStr for Role {
+    fn from_str(s: &str) -> Result<Self, EnumCoercionError> {
+        match s {
+            "ADMIN" => Ok(Self::Admin),
+            "CEO" => Ok(Self::Ceo),
+            "COORD" => Ok(Self::Coord),
+            "EDITOR" => Ok(Self::Editor),
+            "PRINCIPAL" => Ok(Self::Principal),
+            "USER" => Ok(Self::User),
+            "WRITTER" => Ok(Self::Writter),
+            _ => Err(EnumCoercionError::new("Role"))
+        }
+    }
+    
+    type Err = EnumCoercionError;
 }
