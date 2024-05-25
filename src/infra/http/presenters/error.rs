d5a8dd457d524dc43f4ac5b1e4ba4c7ab531ae80
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-25 00:08:03 -0300

    chore: add error presenter

---
+++ src/infra/http/presenters/error.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+use http::StatusCode;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value as JsonValue};
+use validator::ValidationError;
+
+use crate::errors::error::DomainErrorTrait;
+
+#[derive(Serialize, Deserialize)]
+pub struct MappedError {
+    code: u16,
+    message: String,
+}
+
+pub struct ErrorPresenter;
+
+impl ErrorPresenter {
+    pub fn to_http(error: Box<dyn DomainErrorTrait>) -> MappedError {
+        MappedError {
+            code: error.code().to_owned(),
+            message: error.message().to_owned()
+        }
+    }
+
+    pub fn to_http_from_validator(errors: HashMap<&'static str, &Vec<ValidationError>>) -> JsonValue {
+        return json!({
+            "code": StatusCode::BAD_REQUEST.as_u16(),
+            "errors": errors
+        });
+    }
+}
