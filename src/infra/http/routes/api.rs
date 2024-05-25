4e127cf24ec63b2d48c192c2fc24e4f88e704759
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-24 23:27:05 -0300

    fix: register session controller

--- src/infra/http/routes/api.rs
+++ src/infra/http/routes/api.rs
@@ -1,6 +1,7 @@
 use actix_web::web;
 
 use crate::infra::http::controllers::controller::ControllerTrait;
+use crate::infra::http::controllers::sessions_controller::SessionsController;
 use crate::infra::http::middlewares::RequestUserMiddleware;
 use crate::infra::http::controllers::users_controller::UsersController;
 use crate::infra::http::routes::route::RouteTrait;
@@ -13,6 +14,7 @@ impl RouteTrait for ApiRoutes {
         web::scope("api")
             .wrap(RequestUserMiddleware)
             .configure(UsersController::register)
+            .configure(SessionsController::register)
         );
     }
 }
