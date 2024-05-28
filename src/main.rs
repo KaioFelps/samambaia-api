1759525b4eb2d74c1d5ae684cb5b75e9e50acbd6
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-28 02:13:46 -0300

    build: setup env vars to server binding

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::HttpServer;
 use dotenvy::dotenv;
 use env_logger::{self, Target};
-use hubbitos_backend::server::ServerFactory;
+use hubbitos_backend::{server::ServerFactory, ENV_VARS};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,7 +10,8 @@ async fn main() -> std::io::Result<()> {
     env_logger::Builder::new().parse_env("RUST_LOG").target(Target::Stdout).init();
 
     HttpServer::new(ServerFactory::exec)
-    .bind(("127.0.0.1", 3000))?
+    .bind((ENV_VARS.host.as_str(), ENV_VARS.port))?
+    .workers(ENV_VARS.workers)
     .run()
     .await
 }
