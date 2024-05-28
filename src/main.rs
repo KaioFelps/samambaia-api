27ec805245d0240967679790ba3d2b2f6dbb8cf1
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-28 03:14:15 -0300

    feat: apply migrations on server start

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use actix_web::HttpServer;
 use dotenvy::dotenv;
 use env_logger::{self, Target};
-use hubbitos_backend::{server::ServerFactory, ENV_VARS};
+use hubbitos_backend::{infra::sea::sea_service::SeaService, server::ServerFactory, ENV_VARS};
+use log::error;
+use migration::{Migrator, MigratorTrait};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     env_logger::Builder::new().parse_env("RUST_LOG").target(Target::Stdout).init();
+
+    let migration_result = Migrator::up(&SeaService::new().await.db, None).await;
+
+    if migration_result.is_err() {
+        let err = migration_result.unwrap_err();
+
+        error!(
+            "Error occurred on applying pending migrations: \n{}\n",
+            err
+        );
+    }
 
     HttpServer::new(ServerFactory::exec)
     .bind((ENV_VARS.host.as_str(), ENV_VARS.port))?
