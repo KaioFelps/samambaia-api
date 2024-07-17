7f7cf86ee019cf1be4d98e55c46210e63f103892
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-07-17 02:58:43 -0300

    fix: presenter trait

---
+++ src/infra/http/presenters/presenter.rs
@@ -0,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+use crate::infra::http::presenters::pagination::MappedPagination;
+
+pub trait PresenterTrait<Entity, MappedEntity> {
+    #[allow(clippy::wrong_self_convention)]
+    fn to_http(entity: Entity) -> MappedEntity;
+
+    #[allow(clippy::wrong_self_convention)]
+    fn to_json_paginated_wrapper(entities: Vec<MappedEntity>, pagination: MappedPagination) -> JsonWrappedPaginatedEntity<MappedEntity> {
+        JsonWrappedPaginatedEntity {
+            pagination,
+            data: entities
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct JsonWrappedEntity<MappedEntity> {
+    pub data: MappedEntity
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct JsonWrappedPaginatedEntity<MappedEntity> {
+    pub data: Vec<MappedEntity>,
+    pub pagination: MappedPagination
+}
+
