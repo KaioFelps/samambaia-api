4fa71d986fe96efff2e77cf870e4978abbe82fad
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-07-28 18:54:57 -0300

    feat: Added Create Free Badge Presenter

---
+++ src/infra/http/presenters/free_badge.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+use chrono::NaiveDateTime;
+use uuid::Uuid;
+use crate::domain::domain_entities::free_badge::FreeBadge;
+use crate::infra::http::presenters::presenter::PresenterTrait;
+
+#[derive(Serialize, Deserialize)]
+pub struct MappedFreeBadge {
+    pub id: Uuid,
+    pub code: String,
+    pub link: String,
+    #[serde(rename="linkIsExternal")]
+    pub link_is_external: bool,
+    #[serde(rename="availableUntil")]
+    pub available_until: Option<NaiveDateTime>,
+    pub image: String
+}
+
+pub struct FreeBadgePresenter;
+
+impl PresenterTrait<FreeBadge, MappedFreeBadge> for FreeBadgePresenter {
+    fn to_http(free_badge: FreeBadge) -> MappedFreeBadge {
+        MappedFreeBadge {
+            id: free_badge.id(),
+            code: free_badge.code().into(),
+            link: free_badge.link().into(),
+            image: free_badge.image().into(),
+            available_until: free_badge.available_until(),
+            link_is_external: free_badge.link_is_external()
+        }
+    }
+}
