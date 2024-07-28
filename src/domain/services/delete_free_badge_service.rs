f75fa54d02d293177d2265ba691e6833f8326771
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-07-28 18:35:26 -0300

    feat: Added Delete Free Badge service + test + factory

---
+++ src/domain/services/delete_free_badge_service.rs
@@ -0,0 +1,106 @@
+use uuid::Uuid;
+use crate::domain::domain_entities::role::Role;
+use crate::domain::repositories::free_badge_repository::FreeBadgeRepositoryTrait;
+use crate::errors::error::DomainErrorTrait;
+use crate::errors::resource_not_found::ResourceNotFoundError;
+use crate::errors::unauthorized_error::UnauthorizedError;
+use crate::util::{generate_service_internal_error, RolePermissions, verify_role_has_permission};
+
+pub struct DeleteFreeBadgeParams {
+    pub user_role: Role,
+    pub free_badge_id: Uuid
+}
+
+pub struct DeleteFreeBadgeService<FreeBadgeRepository: FreeBadgeRepositoryTrait> {
+    free_badge_repository: FreeBadgeRepository
+}
+
+impl<FreeBadgeRepository: FreeBadgeRepositoryTrait> DeleteFreeBadgeService<FreeBadgeRepository> {
+    pub fn new(free_badge_repository: FreeBadgeRepository) -> Self {
+        DeleteFreeBadgeService {
+            free_badge_repository
+        }
+    }
+
+    pub async fn exec(&self, params: DeleteFreeBadgeParams) -> Result<(), Box<dyn DomainErrorTrait>> {
+        let user_can_delete_free_badge = verify_role_has_permission(&params.user_role, RolePermissions::DeleteFreeBadge);
+
+        if !user_can_delete_free_badge {
+            return Err(Box::new(UnauthorizedError::new()));
+        }
+
+        let free_badge = self.free_badge_repository.find_by_id(params.free_badge_id).await;
+
+        if free_badge.is_err() {
+            return Err(generate_service_internal_error(
+                "Error occurred in Delete Free Badge Service, on finding free badge by id from the database".into(),
+                &free_badge.unwrap_err()
+            ));
+        }
+
+        let free_badge = free_badge.unwrap();
+
+        if free_badge.is_none() {
+            return Err(Box::new(ResourceNotFoundError::new()));
+        }
+
+        let free_badge = free_badge.unwrap();
+
+        let deleted = self.free_badge_repository.delete(free_badge).await;
+
+        if deleted.is_err() {
+            return Err(generate_service_internal_error(
+                "Error occurred in Delete Free Badge Service, on deleting free badge from database".into(),
+                &deleted.unwrap_err()
+            ));
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::domain::domain_entities::free_badge::FreeBadge;
+    use crate::domain::domain_entities::role::Role;
+    use crate::domain::services::delete_free_badge_service::DeleteFreeBadgeParams;
+    use crate::tests::repositories::free_badge_repository::get_free_badge_repository;
+
+    #[tokio::test]
+    async fn test_if_allowed_user_can_delete_free_badge() {
+        let (badges_db, badge_repository) = get_free_badge_repository();
+        let sut = super::DeleteFreeBadgeService::new(badge_repository);
+
+        let badge = FreeBadge::new("PT001".to_string(), "image".to_string(), "link".to_string(), false, None);
+        let badge_id = badge.id();
+
+        badges_db.lock().unwrap().push(badge);
+
+        let result = sut.exec(DeleteFreeBadgeParams {
+            free_badge_id: badge_id,
+            user_role: Role::Writer
+        }).await;
+
+        assert!(result.is_ok());
+        assert_eq!(badges_db.lock().unwrap().len(), 0);
+    }
+
+    #[tokio::test]
+    async fn test_if_not_allowed_user_cannot_delete_free_badge() {
+        let (badges_db, badge_repository) = get_free_badge_repository();
+        let sut = super::DeleteFreeBadgeService::new(badge_repository);
+
+        let badge = FreeBadge::new("PT001".to_string(), "image".to_string(), "link".to_string(), false, None);
+        let badge_id = badge.id();
+
+        badges_db.lock().unwrap().push(badge);
+
+        let result = sut.exec(DeleteFreeBadgeParams {
+            free_badge_id: badge_id,
+            user_role: Role::User
+        }).await;
+
+        assert!(result.is_err());
+        assert_eq!(badges_db.lock().unwrap().len(), 1);
+    }
+}
--- src/domain/services/mod.rs
+++ src/domain/services/mod.rs
@@ -43,3 +43,4 @@ pub mod delete_article_tag_service;
 pub mod create_free_badge_service;
 pub mod update_free_badge_service;
 pub mod fetch_many_free_badges_service;
+pub mod delete_free_badge_service;
