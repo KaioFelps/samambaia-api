d283658dfa767a6016e3c2d001d55034dec59c49
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-11-21 10:11:43 -0300

    fix: service internal error factory

--- src/util/service_internal_error_factory.rs
+++ src/util/service_internal_error_factory.rs
@@ -1,18 +1,12 @@
-use crate::{
-    errors::{error::DomainErrorTrait, internal_error::InternalError},
-    LOG_SEP, R_EOL,
-};
+use crate::{error::DomainError, LOG_SEP, R_EOL};
 use log::error;
 use std::error::Error;
 
-pub fn generate_service_internal_error(
-    message: &str,
-    error: Box<dyn Error>,
-) -> Box<dyn DomainErrorTrait> {
+pub fn generate_service_internal_error(message: &str, error: Box<dyn Error>) -> DomainError {
     error!(
         "{R_EOL}{LOG_SEP}{R_EOL}{}: {R_EOL}{}{R_EOL}{LOG_SEP}{R_EOL}",
         message, error
     );
 
-    Box::new(InternalError::new())
+    DomainError::internal_err()
 }
