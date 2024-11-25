07930b1c1b76fa299a5c20ada41cdc367959d5df
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-11-24 22:02:32 -0300

    feat: add CreateAnnouncementService + unit test

---
+++ src/domain/services/announcements/mod.rs
@@ -0,0 +1,1 @@
+pub mod create_announcement_service;
--- src/domain/services/mod.rs
+++ src/domain/services/mod.rs
@@ -44,3 +44,5 @@ pub mod create_free_badge_service;
 pub mod delete_free_badge_service;
 pub mod fetch_many_free_badges_service;
 pub mod update_free_badge_service;
+
+pub mod announcements;
