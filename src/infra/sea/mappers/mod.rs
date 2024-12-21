76eeafcbe125ccd72c0e7cb7ffe61dc625089e35
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-12-21 17:37:40 -0300

    feat: add `SeaMapper` trait defining 4 most common mapping operations + add `SeaAnnouncementMapper` (implementing `SeaMapper`)

--- src/infra/sea/mappers/mod.rs
+++ src/infra/sea/mappers/mod.rs
@@ -1,3 +1,4 @@
+pub mod sea_announcement_mapper;
 pub mod sea_article_mapper;
 pub mod sea_article_tag_mapper;
 pub mod sea_comment_mapper;
@@ -8,3 +9,10 @@ pub mod sea_role_mapper;
 pub mod sea_team_role_mapper;
 pub mod sea_team_user_mapper;
 pub mod sea_user_mapper;
+
+pub trait SeaMapper<Entity, Model, ActiveModel> {
+    fn entity_into_model(entity: Entity) -> Model;
+    fn entity_into_active_model(entity: Entity) -> ActiveModel;
+    fn active_model_into_entity(active_model: ActiveModel) -> Entity;
+    fn model_into_entity(model: Model) -> Entity;
+}
---
+++ src/infra/sea/mappers/sea_announcement_mapper.rs
@@ -0,0 +1,61 @@
+use super::SeaMapper;
+use crate::domain::domain_entities::announcement::Announcement;
+use entities::announcement::ActiveModel as AnnouncementActiveModel;
+use entities::announcement::Model as AnnouncementModel;
+use sea_orm::IntoActiveValue;
+
+pub struct SeaAnnouncementMapper;
+
+impl SeaMapper<Announcement, AnnouncementModel, AnnouncementActiveModel> for SeaAnnouncementMapper {
+    fn entity_into_model(entity: Announcement) -> AnnouncementModel {
+        AnnouncementModel {
+            author_id: *entity.author_id(),
+            created_at: *entity.created_at(),
+            description: entity.description().to_owned(),
+            external: *entity.external(),
+            id: *entity.id(),
+            image: entity.image().to_owned(),
+            updated_at: *entity.updated_at(),
+            url: entity.url().to_owned(),
+        }
+    }
+
+    fn entity_into_active_model(entity: Announcement) -> AnnouncementActiveModel {
+        AnnouncementActiveModel {
+            id: entity.id().into_active_value(),
+            author_id: entity.author_id().into_active_value(),
+            description: entity.description().to_owned().into_active_value(),
+            url: entity.url().to_owned().into_active_value(),
+            image: entity.image().to_owned().into_active_value(),
+            external: entity.external().into_active_value(),
+            created_at: entity.created_at().into_active_value(),
+            updated_at: entity.updated_at().into_active_value(),
+        }
+    }
+
+    fn active_model_into_entity(active_model: AnnouncementActiveModel) -> Announcement {
+        Announcement::new_from_existing(
+            active_model.id.unwrap(),
+            active_model.url.unwrap(),
+            active_model.image.unwrap(),
+            active_model.external.unwrap(),
+            active_model.description.unwrap(),
+            active_model.created_at.unwrap(),
+            active_model.updated_at.unwrap(),
+            active_model.author_id.unwrap(),
+        )
+    }
+
+    fn model_into_entity(model: AnnouncementModel) -> Announcement {
+        Announcement::new_from_existing(
+            model.id,
+            model.url,
+            model.image,
+            model.external,
+            model.description,
+            model.created_at,
+            model.updated_at,
+            model.author_id,
+        )
+    }
+}
