544247352b778d6ff90ecedbecfcc9832ae081ff
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-07 18:29:03 -0300

    build: add more required environment variables

--- src/env_config.rs
+++ src/env_config.rs
@@ -4,13 +4,22 @@ use dotenvy::dotenv;
 use serde::Deserialize;
 use serde_envfile::from_env as lib_from_env;
 
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum RustEnv {
+    Production,
+    Development,
+}
+
 #[derive(Debug, Deserialize)]
 pub struct EnvConfig {
+    pub rust_env: RustEnv,
     pub database_url: String,
     pub domain: String,
     pub jwt_secret: String,
     pub host: String,
     pub port: u16,
+    pub https: bool,
     pub workers: usize,
 }
 
