5c966eca4589cadbb9d92053bc79f0030bb3036b
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-08 20:03:17 -0300

    chore: add semi rule for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import config from "@rocketseat/eslint-config/react.mjs";
+import stylisticTs from "@stylistic/eslint-plugin-ts";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
   ...config,
   {
     plugins: {
+      "@stylistic/ts": stylisticTs,
       ...config.plugins,
       "simple-import-sort": simpleImportSort,
     },
@@ -16,16 +18,25 @@ export default [
       "@stylistic/jsx-first-prop-new-line": ["error", "multiprop"],
       "@stylistic/jsx-function-call-newline": ["error", "always"],
       "@stylistic/semi": ["error", "always"],
+      "@stylistic/ts/semi": ["error", "always"],
       "@stylistic/jsx-quotes": ["error", "prefer-double"],
       "@stylistic/quotes": ["error", "double"],
       "@stylistic/jsx-max-props-per-line": ["error", {
         maximum: 1,
       }],
       "@stylistic/max-len": ["warn", 100],
-      "@typescript-eslint/no-unused-vars": [
-        "warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-      ],
       "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "@stylistic/ts/member-delimiter-style": ["error", {
+        multiline: {
+          delimiter: "semi",
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: "semi",
+          requireLast: false,
+        },
+        multilineDetection: "brackets",
+      }],
     },
   },
 ];
