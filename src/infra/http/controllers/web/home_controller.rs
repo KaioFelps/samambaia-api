6c5454f70bbed6055ce064f1ab9923300ce1e52c
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-07 21:42:54 -0300

    chore: add home page

--- src/infra/http/controllers/web/home_controller.rs
+++ src/infra/http/controllers/web/home_controller.rs
@@ -6,7 +6,14 @@ use crate::infra::http::controllers::controller::ControllerTrait;
 pub struct HomeController;
 
 impl ControllerTrait for HomeController {
-    fn register(cfg: &mut web::ServiceConfig) {}
+    fn register(cfg: &mut web::ServiceConfig) {
+        cfg.route("/", web::get().to(Self::home));
+    }
 }
 
-impl HomeController {}
+impl HomeController {
+    async fn home(req: HttpRequest) -> impl Responder {
+        println!("executou");
+        Inertia::render(&req, "index".into()).await
+    }
+}
---
+++ www/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Index() {
+  return (
+    <h1>Live Cosmic!</h1>
+  );
+}
