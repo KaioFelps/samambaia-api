0c4beef59460e5e05a979e856d6b2d46f23eb967
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-09 15:50:14 -0300

    remove: static assets route serving at "/assets", since it's no longer needed due to `Vite` assets prefix

--- src/infra/http/routes/web.rs
+++ src/infra/http/routes/web.rs
@@ -1,6 +1,4 @@
-use std::path::Path;
-use std::sync::Arc;
-
+use super::route::RouteTrait;
 use crate::core::pagination::DEFAULT_PER_PAGE;
 use crate::domain::factories::announcements::fetch_many_announcements_service_factory;
 use crate::domain::services::announcements::fetch_many_announcements_service::FetchManyAnnouncementsParams;
@@ -14,21 +12,12 @@ use actix_web::web::{self, Data};
 use inertia_rust::actix::InertiaMiddleware;
 use inertia_rust::{hashmap, prop_resolver, InertiaProp};
 use serde_json::to_value;
-
-use super::route::RouteTrait;
+use std::sync::Arc;
 
 pub struct WebRoutes;
 
 impl RouteTrait for WebRoutes {
     fn register(cfg: &mut web::ServiceConfig) {
-        // adds this service only if the directory exists
-        if Path::new("public/bundle/assets").exists() {
-            // serves vite assets from /assets path
-            cfg.service(
-                actix_files::Files::new("/assets", "./public/bundle/assets").prefer_utf8(true),
-            );
-        }
-
         cfg.service(
             web::scope("")
                 .wrap(RequestUserMiddleware)
