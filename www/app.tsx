8253c3fb8f570b5ea65a0f13753dc4a9219a17ef
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-08 23:42:45 -0300

    fix(frontend): progress bar color in `app.tsx`

--- www/app.tsx
+++ www/app.tsx
@@ -1,6 +1,6 @@
 import "./app.css";
 
-import config from "@crate/tailwind.config";
+import { colors } from "@crate/tailwind.config";
 import { createInertiaApp } from "@inertiajs/react";
 import { ReactNode } from "react";
 import { createRoot, hydrateRoot } from "react-dom/client";
@@ -12,7 +12,7 @@ const appName = import.meta.env.VITE_APP_NAME ?? "Live Cosmic";
 const production = import.meta.env.VITE_RUST_ENV === "PRODUCTION";
 
 createInertiaApp({
-  progress: { color: config.theme.colors["purple-500"], includeCSS: true },
+  progress: { color: colors.purple[500], includeCSS: true },
 
   title: (title) => resolveTitle(title, appName),
 
