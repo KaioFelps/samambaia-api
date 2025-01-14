3f9dab6aa864f698ea8d734781d163088b79037c
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-14 01:40:04 -0300

    fix: home controller

--- src/infra/http/controllers/web/home_controller.rs
+++ src/infra/http/controllers/web/home_controller.rs
@@ -8,10 +8,7 @@ use crate::{
     domain::factories::journalism::articles::fetch_home_page_articles_service_factory,
     error::IntoDomainError,
     infra::{
-        http::{
-            controllers::{controller::ControllerTrait, AppResponse},
-            presenters::{article::ArticlePresenter, presenter::PresenterTrait},
-        },
+        http::controllers::{controller::ControllerTrait, AppResponse},
         sea::sea_service::SeaService,
     },
 };
@@ -27,12 +24,7 @@ impl ControllerTrait for HomeController {
 impl HomeController {
     async fn home(req: HttpRequest, db_conn: Data<SeaService>) -> AppResponse {
         let articles_service = fetch_home_page_articles_service_factory::exec(&db_conn);
-        let articles = articles_service
-            .exec()
-            .await?
-            .into_iter()
-            .map(ArticlePresenter::to_http)
-            .collect::<Vec<_>>();
+        let articles = articles_service.exec().await?;
 
         Inertia::render_with_props(
             &req,
