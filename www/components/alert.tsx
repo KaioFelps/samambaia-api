a804bafc4e6fede3ad1f3fb9bea6b7193d499990
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-16 01:16:50 -0300

    feat: add `Alert` component

---
+++ www/components/alert.tsx
@@ -0,0 +1,37 @@
+import clsx from "clsx";
+
+import { Sprite, SpriteProps } from "./sprite";
+
+type AlertProps = {
+  type: "warning";
+  message: string;
+};
+
+export function Alert({ message, type }: AlertProps) {
+  const sprites: Record<string, SpriteProps> = {
+    warning: {
+      x: -431,
+      y: -5,
+      width: 23,
+      height: 21,
+    },
+  };
+
+  return (
+    <div className={clsx(
+      "w-full flex items-center gap-3 p-2 rounded-lg mx-auto mt-3 border-2 cursor-default",
+      type === "warning" && "bg-yellow-500/25 border-yellow-900 text-yellow-900",
+    )}
+    >
+      <div className={clsx(
+        "size-[40px] grid place-items-center rounded-md",
+        type === "warning" && "bg-yellow-900",
+      )}
+      >
+        <Sprite {...sprites[type]} />
+      </div>
+
+      {message}
+    </div>
+  );
+}
