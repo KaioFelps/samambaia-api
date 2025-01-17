f75e4f13352c617fe0e0ea825fb6f5572b63228d
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-17 17:20:22 -0300

    feat: add `GarbageCollectorMiddleware`

---
+++ src/infra/http/middlewares/gargabe_collector.rs
@@ -0,0 +1,60 @@
+use actix_web::dev::{self, Service, ServiceRequest, ServiceResponse, Transform};
+use actix_web::Error;
+use actix_web_lab::__reexports::futures_util::future::LocalBoxFuture;
+use rand::Rng;
+use std::future::{ready, Ready};
+
+use crate::configs::app::APP_CONFIG;
+use crate::configs::file_sessions::clean_expired_sessions;
+
+pub struct GarbageCollectorMiddleware;
+impl<S, B> Transform<S, ServiceRequest> for GarbageCollectorMiddleware
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Transform = GarbageCollectorMiddlewareTransform<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(GarbageCollectorMiddlewareTransform { service }))
+    }
+}
+
+pub struct GarbageCollectorMiddlewareTransform<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for GarbageCollectorMiddlewareTransform<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    dev::forward_ready!(service);
+
+    fn call(&self, request: ServiceRequest) -> Self::Future {
+        let lottery_num = rand::thread_rng().gen_range(0..APP_CONFIG.lottery[1]);
+        let is_raffled = lottery_num <= APP_CONFIG.lottery[0];
+
+        let fut = self.service.call(request);
+
+        Box::pin(async move {
+            let res = fut.await?;
+
+            if is_raffled {
+                let _ = clean_expired_sessions().await;
+            }
+
+            Ok(res)
+        })
+    }
+}
--- src/infra/http/middlewares/mod.rs
+++ src/infra/http/middlewares/mod.rs
@@ -1,6 +1,8 @@
 mod authentication;
+mod gargabe_collector;
 mod request_user;
 
 pub use authentication::authentication_middleware;
 pub use authentication::AuthenticationMiddleware;
+pub use gargabe_collector::GarbageCollectorMiddleware;
 pub use request_user::RequestUserMiddleware;
