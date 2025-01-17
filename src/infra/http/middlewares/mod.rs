cc222dceb358e754c25e70aee81b675822841df6
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-17 18:35:38 -0300

    feat: add Inertia Rust's `ReflashTemporarySessionMiddleware`

--- src/infra/http/middlewares/mod.rs
+++ src/infra/http/middlewares/mod.rs
@@ -1,8 +1,10 @@
 mod authentication;
 mod gargabe_collector;
+mod inertia_temporary_session;
 mod request_user;
 
 pub use authentication::authentication_middleware;
 pub use authentication::AuthenticationMiddleware;
 pub use gargabe_collector::GarbageCollectorMiddleware;
+pub use inertia_temporary_session::ReflashTemporarySessionMiddleware;
 pub use request_user::RequestUserMiddleware;
