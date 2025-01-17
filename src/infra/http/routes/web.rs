af7db80614b655ed95fd8a8bdd683e08aeaed8e8
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-17 18:35:56 -0300

    feat: setup session related middlewares

--- src/infra/http/routes/web.rs
+++ src/infra/http/routes/web.rs
@@ -1,20 +1,27 @@
 use super::route::RouteTrait;
 use crate::configs::app::APP_CONFIG;
 use crate::configs::env::RustEnv;
+use crate::configs::file_sessions::FileSessionStore;
 use crate::core::pagination::DEFAULT_PER_PAGE;
 use crate::domain::factories::announcements::fetch_many_announcements_service_factory;
 use crate::domain::services::announcements::fetch_many_announcements_service::FetchManyAnnouncementsParams;
 use crate::infra::http::controllers::controller::ControllerTrait;
 use crate::infra::http::controllers::web::home_controller::HomeController;
-use crate::infra::http::middlewares::RequestUserMiddleware;
+use crate::infra::http::middlewares::{
+    GarbageCollectorMiddleware, ReflashTemporarySessionMiddleware, RequestUserMiddleware,
+};
 use crate::infra::http::presenters::announcement::AnnouncementPresenter;
 use crate::infra::http::presenters::presenter::PresenterTrait;
 use crate::infra::sea::sea_service::SeaService;
+use actix_session::SessionMiddleware;
 use actix_web::body::BoxBody;
+use actix_web::cookie::{Key, SameSite};
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::http::StatusCode;
 use actix_web::middleware::{from_fn, Next};
 use actix_web::web::{self, Data};
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
 use inertia_rust::actix::InertiaMiddleware;
 use inertia_rust::{
     hashmap, prop_resolver, Inertia, InertiaFacade, InertiaProp, IntoInertiaError,
@@ -27,8 +34,17 @@ pub struct WebRoutes;
 
 impl RouteTrait for WebRoutes {
     fn register(cfg: &mut web::ServiceConfig) {
+        let key_bytes = BASE64_STANDARD
+            .decode(APP_CONFIG.app_key)
+            .expect("Invalid APP_KEY value.");
+
+        let key = Key::derive_from(key_bytes.as_slice());
+
+        let storage = FileSessionStore::default();
+
         cfg.service(
             web::scope("")
+                .wrap(GarbageCollectorMiddleware)
                 .wrap(from_fn(default_error_handler))
                 .wrap(RequestUserMiddleware)
                 .wrap(InertiaMiddleware::new().with_shared_props(Arc::new(|req| {
@@ -70,6 +86,14 @@ impl RouteTrait for WebRoutes {
                         ]
                     })
                 })))
+                .wrap(ReflashTemporarySessionMiddleware)
+                .wrap(SessionMiddleware::builder(storage, key)
+                    .cookie_domain(Some(APP_CONFIG.domain.into()))
+                    .cookie_http_only(true)
+                    .cookie_same_site(SameSite::Strict)
+                    .cookie_name(APP_CONFIG.session_cookie.into())
+                    .cookie_secure(APP_CONFIG.rust_env == RustEnv::Production)
+                    .build())
                 .configure(HomeController::register)
                 .configure(|cfg| {
                     // serves public assets directly from /path
