05f52dd1abe209147e739081d407f76130938b33
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-19 17:58:45 -0300

    chore: add traits for mapping errors to Inertia redirects with errors

--- src/configs/inertia.rs
+++ src/configs/inertia.rs
@@ -5,11 +5,17 @@ use crate::{
 };
 
 use super::vite::initialize_vite;
-use inertia_rust::{
-    template_resolvers::ViteTemplateResolver, Inertia, InertiaConfig, InertiaError, InertiaVersion,
-    IntoInertiaError,
+use actix_web::{
+    web::{self, Redirect},
+    HttpRequest,
 };
-use std::io;
+use inertia_rust::{
+    hashmap, template_resolvers::ViteTemplateResolver, Inertia, InertiaConfig, InertiaError,
+    InertiaFacade, InertiaVersion, IntoInertiaError,
+};
+use serde::de::DeserializeOwned;
+use std::{collections::HashMap, io};
+use validator::Validate;
 
 pub async fn initialize_inertia() -> Result<Inertia, io::Error> {
     let vite = initialize_vite().await;
@@ -49,5 +55,72 @@ impl IntoSamambaiaError for InertiaError {
 impl IntoInertiaError for SamambaiaError {
     fn into_inertia_error(self) -> InertiaError {
         InertiaError::RenderError(self.to_string())
+    }
+}
+
+pub trait IntoInertiaRedirect<TRequest, TRedirect> {
+    fn into_inertia_redirect(self, req: &TRequest) -> TRedirect;
+}
+
+pub trait InertiaValidateOrRedirect<T, TRequest, TRedirect>
+where
+    T: DeserializeOwned + Validate,
+{
+    fn validate_or_back(self, req: &TRequest) -> Result<T, TRedirect>;
+}
+
+impl<T: DeserializeOwned + Validate> InertiaValidateOrRedirect<T, HttpRequest, Redirect> for T {
+    #[inline]
+    fn validate_or_back(self, req: &HttpRequest) -> Result<T, Redirect> {
+        match self.validate() {
+            Ok(_) => Ok(self),
+            Err(errors) => {
+                let errors = errors
+                    .field_errors()
+                    .into_iter()
+                    .map(|(key, v)| {
+                        (
+                            key,
+                            v[0].message.as_ref().unwrap_or(&v[0].code).clone().into(),
+                        )
+                    })
+                    .collect::<HashMap<_, _>>();
+
+                println!("{:#?}", errors);
+
+                Err(Inertia::back_with_errors(req, errors))
+            }
+        }
+    }
+}
+
+impl<T: DeserializeOwned + Validate> InertiaValidateOrRedirect<T, HttpRequest, Redirect>
+    for web::Json<T>
+{
+    #[inline]
+    fn validate_or_back(self, req: &HttpRequest) -> Result<T, Redirect> {
+        self.into_inner().validate_or_back(req)
+    }
+}
+
+impl IntoInertiaRedirect<HttpRequest, Redirect> for SamambaiaError {
+    fn into_inertia_redirect(self, req: &HttpRequest) -> Redirect {
+        Inertia::back_with_errors(
+            req,
+            hashmap![
+                "error" => self.get_message().into()
+            ],
+        )
+    }
+}
+
+impl<T: IntoSamambaiaError> IntoInertiaRedirect<HttpRequest, Redirect> for T {
+    fn into_inertia_redirect(self, req: &HttpRequest) -> Redirect {
+        Inertia::back_with_errors(
+            req,
+            hashmap![
+                "error" => self.into_samambaia_error().get_message().into()
+            ],
+        )
     }
 }
