967a0bec73c45c3fa0befdc2f6c420bd7c34362a
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-19 18:05:44 -0300

    refactor: remove unused imports

--- src/infra/http/controllers/web/home_controller.rs
+++ src/infra/http/controllers/web/home_controller.rs
@@ -18,10 +18,17 @@ pub struct HomeController;
 impl ControllerTrait for HomeController {
     fn register(cfg: &mut web::ServiceConfig) {
         cfg.route("/", web::get().to(Self::home));
+        cfg.route("foo", web::get().to(Self::foo));
     }
 }
 
 impl HomeController {
+    async fn foo(req: HttpRequest) -> AppResponse {
+        Inertia::render(&req, "Foo".into())
+            .await
+            .map_err(IntoSamambaiaError::into_samambaia_error)
+    }
+
     async fn home(req: HttpRequest, db_conn: Data<SeaService>) -> AppResponse {
         let articles_service = fetch_home_page_articles_service_factory::exec(&db_conn);
         let articles = articles_service.exec().await?;
