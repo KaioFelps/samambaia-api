7f94fd2146da042ad8938f7bf546701d1e85621e
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-20 17:15:22 -0300

    feat: enhance SEO with meta tags and SSR

--- www/config/app.ts
+++ www/config/app.ts
@@ -1,7 +1,18 @@
+import cover from "@/assets/website-meta-cover.jpeg";
+
+const appName = "Live Cosmic" as const;
+
 export const appConfig = {
-  appName: "Live Cosmic",
+  appName,
   imagerUrl: "https://habblive.in/imager.php",
   userInfoUrl: "https://habblive.in/api/userinfo.php",
+
+  meta: {
+    appURL: "https://live-cosmic-staging.squareweb.app/",
+    title: appName,
+    description: "Sua galáxia de entretenimento e informações no Habblive Hotel!",
+    cover,
+  },
 } as const;
 
 Object.freeze(appConfig);
