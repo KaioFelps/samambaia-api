c45cac65d0d73d2bda5e3a9e30fe5ef1a5e6a105
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-27 20:48:41 -0300

    feat: add `Head` component (a wrapper around Inertia's Head component) that handles SEO meta tags better

--- www/pages/Error.tsx
+++ www/pages/Error.tsx
@@ -1,6 +1,6 @@
-import { Head } from "@inertiajs/react";
 import { ReactNode } from "react";
 
+import { Head } from "@/components/head";
 import { Sprite } from "@/components/sprite";
 import { RawPublicLayout } from "@/layouts/public";
 
@@ -11,10 +11,10 @@ type ErrorPageProps = {
 export default function ErrorPage({ status }: ErrorPageProps) {
   const messages: Record<number, string> = {
     500: "Estamos com algum probleminha interno!",
-    503: "Nossos serviços não estão disponíveis.",
+    503: "Nossos serviços não estão disponíveis =(",
     404: "Lugar errado?!",
-    403: "Negativo! Acesso negado.",
-    401: "Negativo! Não autorizado.",
+    403: "Acesso negade",
+    401: "Não autorizade",
   };
 
   const descriptions: Record<number, string> = {
@@ -22,18 +22,24 @@ export default function ErrorPage({ status }: ErrorPageProps) {
     500: "Não é você... Somos nós.",
     404: "Talvez você tenha ido longe demais! Dê alguns passos para trás.",
     403: "Eu posso. Você não pode! Você não pode, eu posso!",
-    401: "Você não tem todo esse poder, se ponha no teu lugar.",
+    401: "Você não tem todo esse poder, se toca! 💅🏽✨",
   };
 
   return (
     <>
-      <Head title={messages[status]} />
-      <main className="flex items-center justify-center gap-8 px-6 py-28">
+      <Head
+        title={messages[status]}
+        description={descriptions[status]}
+      />
+      <main className="flex items-center justify-center gap-4 px-6 py-28">
         <div>
-          <h1 className="text-purple-700 font-black text-5xl mb-4 max-w-96 text-balance">
+          <h1 className="
+            text-purple-700 font-black text-5xl mb-4 max-w-96 text-balance font-rowdies
+            "
+          >
             {messages[status] ?? messages[500]}
           </h1>
-          <p className="text-2xl font-medium max-w-96 text-balance">
+          <p className="text-2xl font-medium max-w-[420px] text-balance font-rowdies">
             {descriptions[status] ?? descriptions[500]}
           </p>
         </div>
