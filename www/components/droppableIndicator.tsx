7181cea30a3555413f1ea9527d3801a581ad2c9c
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-27 22:48:47 -0300

    fix: arrow indicator sprite

--- www/components/droppableIndicator.tsx
+++ www/components/droppableIndicator.tsx
@@ -5,10 +5,10 @@ import { Sprite } from "@/components/sprite";
 export function DroppableIndicator({ className }: { className?: string }) {
   return (
     <Sprite
-      x={-185}
+      x={-184}
       y={-62}
       height={20}
-      width={13}
+      width={14}
       className={clsx(
         "rotate-90 group-data-[state=open]:-rotate-90 transition-[transform] duration-150",
         className && className,
