732a51a877f50e471c949ae8d23be4dd7fd2b388
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-30 17:55:47 -0300

    feat: finish admin layout header dropdowns

---
+++ www/components/droppableArrow.tsx
@@ -0,0 +1,8 @@
+export const adminDroppableArrowProps = {
+  className: "fill-purple-100 shadow-gray-400",
+  width: 20,
+  height: 10,
+  style: {
+    filter: "drop-shadow(0 1px 0 var(--tw-shadow-color))",
+  },
+};
--- www/components/droppableIndicator.tsx
+++ www/components/droppableIndicator.tsx
@@ -1,3 +1,4 @@
+import { CaretDown } from "@phosphor-icons/react/dist/ssr/CaretDown";
 import clsx from "clsx";
 
 import { Sprite } from "@/components/sprite";
@@ -11,6 +12,20 @@ export function DroppableIndicator({ className }: { className?: string }) {
       width={14}
       className={clsx(
         "rotate-90 group-data-[state=open]:-rotate-90 transition-[transform] duration-150",
+        className && className,
+      )}
+    />
+  );
+}
+
+export function AdminDroppableIndicator({ className }: { className?: string }) {
+  return (
+    <CaretDown
+      size={14}
+      weight="bold"
+      className={clsx(
+        "group-data-[state=open]:-rotate-180 transition-[transform] duration-150",
+        "text-gray-700",
         className && className,
       )}
     />
