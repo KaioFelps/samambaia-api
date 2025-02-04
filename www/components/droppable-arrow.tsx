37ce88c9677159388b0001aab40faf39500bccdb
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-02-04 18:14:59 -0300

    style: fix mozila css issue in admin layout dropdowns

--- www/components/droppable-arrow.tsx
+++ www/components/droppable-arrow.tsx
@@ -1,22 +1,19 @@
 import clsx from "clsx";
 
 export const adminDroppableArrowProps = {
-  className: "fill-purple-100 shadow-gray-400",
+  className: clsx(
+    "fill-purple-100 shadow-gray-400",
+    "[filter:_drop-shadow(0_1px_0_var(--tw-shadow-color))]",
+    "admin-droppable-arrow-bottom-drop-shadow",
+  ),
   width: 20,
   height: 10,
-  style: {
-    filter: "drop-shadow(0 1px 0 var(--tw-shadow-color))",
-  },
 };
 
 export const publicDroppableArrowProps = {
   className: clsx(
     "fill-gray-800 group-data-[side=bottom]:fill-[#4f4f55]",
-    "group-data-[side=bottom]:drop-shadow-[0_2px_0_black]",
-    "public-droppable-arrow-bottom-drop-shadow",
-    "group-data-[side=top]:drop-shadow-[0_2px_0_black]",
-    "group-data-[side=right]:drop-shadow-[0_2px_0_black]",
-    "group-data-[side=left]:drop-shadow-[0_2px_0_black]",
+    "drop-shadow-[0_2px_0_black] public-droppable-arrow-bottom-drop-shadow",
   ),
   width: 20,
   height: 10,
