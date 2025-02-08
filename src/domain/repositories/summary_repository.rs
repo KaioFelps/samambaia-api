438f960bcdc33d7f7f662674611304edcc0321c9
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-02-08 20:55:25 -0300

    feat: add `SummaryRepositoryTrait`

---
+++ src/domain/repositories/summary_repository.rs
@@ -0,0 +1,13 @@
+use async_trait::async_trait;
+use std::error::Error;
+
+#[cfg(test)]
+use mockall::automock;
+
+use crate::domain::value_objects::count_summary::CountSummary;
+
+#[cfg_attr(test, automock)]
+#[async_trait]
+pub trait SummaryRepositoryTrait {
+    async fn get_table_summary(&self) -> Result<CountSummary, Box<dyn Error>>;
+}
