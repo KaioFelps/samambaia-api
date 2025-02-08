91ac19068dfebbe5ed116234bc5f7f4d4ee75ce5
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-02-08 20:57:38 -0300

    chore: add summary repository mock getter

--- src/tests/repositories/mod.rs
+++ src/tests/repositories/mod.rs
@@ -2,4 +2,5 @@ pub mod announcements_repository;
 pub mod article_repository;
 pub mod article_tag_repository;
 pub mod free_badge_repository;
+pub mod summary_repository;
 pub mod users_repository;
