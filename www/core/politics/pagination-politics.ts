724a59b71d5a990be0f34b5c3f5b4eee5a43bdd7
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-22 02:43:28 -0300

    feat: add Paginator, PaginatorPolitics and unit tests

---
+++ www/core/politics/pagination-politics.ts
@@ -0,0 +1,66 @@
+export type GetVisibleButtonsParams = {
+  currentPage: number;
+  lastPage: number;
+  visibleButtons: number;
+  align: "left" | "center";
+};
+
+export class PaginationPolitics {
+  public static DEFAULT_VISIBLE_BUTTONS = 5;
+  public static MAX_VISIBLE_BUTTONS = 12;
+
+  public static getVisibleButtons({
+    align,
+    lastPage,
+    currentPage,
+    visibleButtons,
+  }: GetVisibleButtonsParams) {
+    if (align === "center") {
+      return PaginationPolitics.getCenteredVisibleButtons(currentPage, lastPage, visibleButtons);
+    }
+
+    return PaginationPolitics.getLeftAlignedVisibleButtons(currentPage, lastPage, visibleButtons);
+  }
+
+  private static getCenteredVisibleButtons(
+    currentPage: number,
+    lastPage: number,
+    visibleButtons: number,
+  ) {
+    let maxLeft = (currentPage - Math.floor(visibleButtons / 2));
+    let maxRight = (currentPage + Math.floor(visibleButtons / 2));
+
+    if (maxLeft <= 1) {
+      maxLeft = 1;
+      maxRight = visibleButtons;
+    }
+
+    if (maxRight >= lastPage) {
+      maxLeft = lastPage - (visibleButtons - 1);
+      maxRight = lastPage;
+    }
+
+    if (lastPage < visibleButtons + 1) {
+      maxLeft = 1;
+      maxRight = lastPage;
+    }
+
+    return {
+      maxLeft,
+      maxRight,
+    };
+  }
+
+  private static getLeftAlignedVisibleButtons(
+    currentPage: number,
+    lastPage: number,
+    visibleButtons: number,
+  ) {
+    const maxLeft = 0;
+    let maxRight = currentPage + visibleButtons;
+
+    if (maxRight > lastPage) maxRight = lastPage;
+
+    return { maxLeft, maxRight };
+  }
+}
