706482123c6aa8de82752bbcf33ef1ab9dc64ed1
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-21 22:07:38 -0300

    change: home article to article preview (real struct)

---
+++ src/domain/domain_entities/article_preview.rs
@@ -0,0 +1,120 @@
+use chrono::NaiveDateTime;
+use serde::Deserialize;
+use uuid::Uuid;
+
+use super::slug::Slug;
+
+#[derive(Deserialize)]
+pub struct ArticlePreviewTag {
+    id: i32,
+    value: String,
+}
+
+impl ArticlePreviewTag {
+    pub fn new(id: i32, value: String) -> Self {
+        Self { id, value }
+    }
+
+    pub fn id(&self) -> i32 {
+        self.id
+    }
+
+    pub fn value(&self) -> &str {
+        &self.value
+    }
+}
+
+#[derive(Deserialize)]
+pub struct ArticlePreviewAuthor {
+    nickname: String,
+    id: Uuid,
+}
+
+impl ArticlePreviewAuthor {
+    pub fn new(id: Uuid, nickname: String) -> Self {
+        Self { nickname, id }
+    }
+
+    pub fn nickname(&self) -> &str {
+        &self.nickname
+    }
+
+    pub fn id(&self) -> &Uuid {
+        &self.id
+    }
+}
+
+#[derive(Deserialize)]
+pub struct ArticlePreview {
+    id: Uuid,
+    cover_url: String,
+    title: String,
+    description: String,
+    approved: bool,
+    tag: Option<ArticlePreviewTag>,
+    author: ArticlePreviewAuthor,
+    created_at: NaiveDateTime,
+    slug: Slug,
+}
+
+impl ArticlePreview {
+    pub fn new(
+        id: Uuid,
+        cover_url: String,
+        title: String,
+        description: String,
+        approved: bool,
+        tag: Option<ArticlePreviewTag>,
+        author: ArticlePreviewAuthor,
+        created_at: NaiveDateTime,
+        slug: Slug,
+    ) -> Self {
+        Self {
+            id,
+            cover_url,
+            title,
+            description,
+            approved,
+            tag,
+            author,
+            created_at,
+            slug,
+        }
+    }
+
+    pub fn id(&self) -> &Uuid {
+        &self.id
+    }
+
+    pub fn cover_url(&self) -> &str {
+        &self.cover_url
+    }
+
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn description(&self) -> &str {
+        &self.description
+    }
+
+    pub fn approved(&self) -> bool {
+        self.approved
+    }
+
+    pub fn tag(&self) -> Option<&ArticlePreviewTag> {
+        self.tag.as_ref()
+    }
+
+    pub fn author(&self) -> &ArticlePreviewAuthor {
+        &self.author
+    }
+
+    pub fn created_at(&self) -> NaiveDateTime {
+        self.created_at.clone()
+    }
+
+    pub fn slug(&self) -> &Slug {
+        &self.slug
+    }
+}
--- src/domain/domain_entities/mod.rs
+++ src/domain/domain_entities/mod.rs
@@ -1,12 +1,12 @@
 pub mod announcement;
 pub mod article;
+pub mod article_preview;
 pub mod article_tag;
 pub mod comment;
 pub mod comment_report;
 pub mod comment_with_author;
 pub mod council_alert;
 pub mod free_badge;
-pub mod home_article;
 pub mod role;
 pub mod slug;
 pub mod team_role;
