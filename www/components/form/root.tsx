cc19e37ce8ec6ac9218f9e3ad3ad17acd4c7e43c
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-25 01:10:43 -0300

    fix(Forrm components): add root component

---
+++ www/components/form/root.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import { FormHTMLAttributes } from "react";
+
+type FormRootProps = FormHTMLAttributes<HTMLFormElement>;
+
+export const FormRoot = ({ className, ...props }: FormRootProps) => {
+  return (
+    <form
+      className={clsx(
+        "bg-white p-6 rounded-xl shadow-md shadow-black/5 w-full",
+        "border border-gray-300",
+        className && className,
+      )}
+      {...props}
+    />
+  );
+};
