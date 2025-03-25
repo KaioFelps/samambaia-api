3ac7313afcf5efcd70e788022ac727749f0d1cff
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-25 01:08:17 -0300

    refactor(form input): add root `Form` component and admin variant of `Input`

--- www/components/form/input.tsx
+++ www/components/form/input.tsx
@@ -1,47 +1,21 @@
-import clsx from "clsx";
 import { DetailedHTMLProps, InputHTMLAttributes } from "react";
 
-type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
+import { AdminInput } from "./admin-input";
+import { PublicInput } from "./public-input";
+
+export type InputProps =
+DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
   label: string;
   validationError?: string;
   containerClassName?: string;
+  admin?: boolean;
 };
 
 export function Input({
-  id,
-  name,
-  label,
-  className,
-  validationError,
-  containerClassName,
-  ...rest
+  admin,
+  ...props
 }: InputProps) {
-  return (
-    <div className={containerClassName}>
-      <label
-        htmlFor={id ?? name}
-        className="block text-sm mb-1 ml-1"
-      >
-        {label}
-      </label>
-
-      {validationError && (
-        <span className="
-          block text-red-700 font-medium mb-2 text-sm bg-red-700/20 py-1 px-2 rounded-md
-          "
-        >
-          {validationError}
-        </span>)}
-
-      <input
-        id={id ?? name}
-        name={name}
-        className={clsx("text-input", className && className)}
-        data-invalid={validationError
-          ? ""
-          : null}
-        {...rest}
-      />
-    </div>
-  );
+  return admin
+    ? <AdminInput {...props} />
+    : <PublicInput {...props} />;
 }
