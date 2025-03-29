abf4874689d607d758222cd201126b1a052926ac
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-28 23:13:26 -0300

    change(Home page): it would show up to 9 articles on home page, now it shows up to 6

--- src/infra/http/controllers/web/home_controller.rs
+++ src/infra/http/controllers/web/home_controller.rs
@@ -2,7 +2,6 @@ use actix_web::web::{self, Data};
 use actix_web::HttpRequest;
 use inertia_rust::{hashmap, Inertia, InertiaFacade, InertiaProp};
 
-use crate::core::pagination::DEFAULT_PER_PAGE;
 use crate::domain::factories::journalism::articles::fetch_articles_previews_service_factory;
 use crate::domain::factories::journalism::free_badges::fetch_many_free_badges_service_factory;
 use crate::domain::services::journalism::articles::fetch_articles_previews_service::FetchArticlesPreviewsResponse;
@@ -43,13 +42,14 @@ impl HomeController {
         query: web::Query<HomeQueryDto>,
     ) -> AppResponse {
         const FREE_BADGES_PER_PAGE: u8 = 26;
+        const ARTICLES_PER_PAGE: u8 = 6;
 
         let articles_service = fetch_articles_previews_service_factory::exec(&db_conn);
         let free_badges_service = fetch_many_free_badges_service_factory::exec(&db_conn);
 
         let articles_future = articles_service.exec(FetchArticlesParams {
             page: None,
-            per_page: None,
+            per_page: Some(ARTICLES_PER_PAGE as u32),
             approved_state: Some(true),
             query: None,
         });
@@ -71,7 +71,7 @@ impl HomeController {
         let articles = ArticlePreviewPresenter::to_json_paginated_wrapper(
             articles.data,
             articles.pagination,
-            DEFAULT_PER_PAGE,
+            ARTICLES_PER_PAGE,
         );
 
         Inertia::render_with_props(
