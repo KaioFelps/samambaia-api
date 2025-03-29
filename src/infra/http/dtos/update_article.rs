148d4ee3d1888f08cf605f947f77e49797716e24
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-29 14:11:24 -0300

    remove(UpdateArticleDto): remove serde rename attribute

--- src/infra/http/dtos/update_article.rs
+++ src/infra/http/dtos/update_article.rs
@@ -4,7 +4,6 @@ use validator::Validate;
 
 #[derive(Serialize, Deserialize, Validate)]
 pub struct UpdateArticleDto {
-    #[serde(rename = "authorId")]
     pub author_id: Option<Uuid>,
 
     pub content: Option<String>,
@@ -12,13 +11,11 @@ pub struct UpdateArticleDto {
     pub description: Option<String>,
 
     #[validate(url(message = "Cover url must be a valid url."))]
-    #[serde(rename = "coverUrl")]
     pub cover_url: Option<String>,
 
     pub title: Option<String>,
 
     pub approved: Option<bool>,
 
-    #[serde(rename = "tagId")]
     pub tag_id: Option<i32>,
 }
