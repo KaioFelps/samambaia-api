a9970da528fca45cdf3a482d2b207026c2458b25
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-03-29 18:53:51 -0300

    remove: remove custom trigger from public dialog component, as it does nothing beyond default trigger

--- www/components/dialog/index.tsx
+++ www/components/dialog/index.tsx
@@ -2,12 +2,11 @@ import * as Dialog from "@radix-ui/react-dialog";
 
 import { DialogContent } from "./content";
 import { DialogHeader } from "./header";
-import { DialogTrigger } from "./trigger";
 
 export default {
   Root: Dialog.Root,
   Content: DialogContent,
   Header: DialogHeader,
-  Trigger: DialogTrigger,
+  Trigger: Dialog.Trigger,
   Close: Dialog.Close,
 };
