9528cdc86413d00df09ed87e59937ac8a1208690
Kaio Felps <kaiofelipe2005@gmail.com>
2025-09-27 02:27:34 -0300

    style: add "pixelated" class to fansite logo in admin layout header

--- www/layouts/admin/index.tsx
+++ www/layouts/admin/index.tsx
@@ -31,7 +31,7 @@ export function AdminLayout({ children, props }: { children: ReactNode; props: P
             "bg-gray-100 px-6 py-2 flex items-center justify-between border-b border-gray-250",
           )}>
           <Link href="/gremio">
-            <img src={appConfig.assets.adminLogo} alt="Cosmic" />
+            <img src={appConfig.assets.adminLogo} alt="Cosmic" className="pixelated" />
           </Link>
           <div className="flex gap-2">
             <CreateShortcutsDropdown />
