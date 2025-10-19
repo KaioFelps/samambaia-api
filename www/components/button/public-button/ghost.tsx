cf60ebbe696229d730b6e5c5fa38e199c829bdb4
Kaio Felps <kaiofelipe2005@gmail.com>
2025-10-18 22:03:23 -0300

    feat(`PublicButton`): add blue variant of ghost public button

--- www/components/button/public-button/ghost.tsx
+++ www/components/button/public-button/ghost.tsx
@@ -4,27 +4,32 @@ import React from "react";
 import type { CorePublicButtonProps } from ".";
 
 type Props = {
-  variant?: "success";
+  variant?: "success" | "blue";
   size?: "md" | "lg";
 } & CorePublicButtonProps;
 
 export const GhostPublicButton = React.forwardRef<HTMLButtonElement, Props>(
-  ({ asChild, children, variant = "success", size = "md", ...props }, ref) => {
+  ({ asChild, children, variant = "success", size = "md", disabled, ...props }, ref) => {
     const Button = asChild ? Slot : "button";
     return (
       <Button
         ref={ref}
+        disabled={disabled}
+        data-disabled={disabled ? "disabled" : "enabled"}
+        role="button"
         className={clsx(
           "flex items-center justify-center gap-3 leading-tight",
           "transition-all duration-100 will-change-[shadow,_filter]",
-          "not-enabled:cursor-not-allowed",
+          "button-like-disabled:cursor-not-allowed",
 
           size === "md" && "px-6 py-2.5 font-medium",
           size === "lg" && "px-8 py-3.5 font-bold",
 
           variant === "success" && "btn-ghost-success",
+          variant === "blue" && "btn-ghost-blue"
         )}
-        {...props}>
+        {...props}
+      >
         {children}
       </Button>
     );
